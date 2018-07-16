6822b107b799a324d0d1392d4c3165d69fca1c90
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-07-16 23:33:42 +0900

    [update] calculate alignment score

--- execution.R
+++ execution.R
@@ -12,3 +12,24 @@ for (i in 2:rowLen) {
   }
   i = i + 1
 }
+
+alignmentScore <- array()
+i <- rowLen
+j <- colLen
+n <- 1
+while (TRUE) {
+  if (i == 1 && j == 1) break
+  alignmentScore[n] <- mat[i, j, 1]
+  n <- n + 1
+
+    trace <- mat[i, j, 2]
+  if (trace == 0) {
+    i <- i - 1
+    j <- j - 1
+  } else if (trace == 1) {
+    i <- i - 1
+  } else if (trace == -1){
+    j <- j - 1
+  }
+}
+alignmentScore <- rev(alignmentScore)
