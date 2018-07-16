af3e8a0c5c464cffd16d9a793107aff08dbfd4fe
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-07-17 01:56:13 +0900

    [refactoring]

--- execution.R
+++ execution.R
@@ -1,3 +1,4 @@
+# calculate matrix for sequence alignment
 mat <- makeMatrix(seq1, seq2)
 mat <- initializeMat(mat, p)
 
@@ -13,17 +14,18 @@ for (i in 2:rowLen) {
   i = i + 1
 }
 
-alignment <- list(NA, NA)
+# trace back
+score <- gap <- c() 
 i <- rowLen
 j <- colLen
 n <- 1
 while (TRUE) {
   if (i == 1 && j == 1) break
-  alignment[[1]][n] <- mat[i, j, 1]
-  alignment[[2]][n] <- mat[i, j, 2]
+  score[n] <- mat[i, j, 1]
+  gap[n] <- mat[i, j, 2]
   n <- n + 1
-
-    trace <- mat[i, j, 2]
+  
+  trace <- mat[i, j, 2]
   if (trace == 0) {
     i <- i - 1
     j <- j - 1
@@ -33,16 +35,15 @@ while (TRUE) {
     j <- j - 1
   }
 }
-score <- rev(alignment[[1]])
-gap <- rev(alignment[[2]])
+score <- rev(score)
+gap <- rev(gap)
 
+# output alignment
 s1 <- seq1[2:length(seq1)]
 s2 <- seq2[2:length(seq2)]
 
-ss1 <- c()
-ss2 <- c()
-i = j = 1
-
+ss1 <- ss2 <- c()
+i <- j <- 1
 for (t in 1:length(gap)) {
   if(gap[t] == 0) {
     ss1 <- append(ss1, s1[i])
