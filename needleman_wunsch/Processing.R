a09c47462a2ccc116fd062837654f1e6eb5a1acf
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-07-23 05:01:44 +0900

    [refactoring] Processing.R

--- needleman_wunsch/Processing.R
+++ needleman_wunsch/Processing.R
@@ -1,5 +1,7 @@
 # data 
 library(openxlsx)
+
+# output sheets
 for (i in 6:6) {
   #for (i in 6:135) {
   sheetNum <- i
@@ -7,31 +9,21 @@ for (i in 6:6) {
   write.table(sheet, paste("sheet-", sheetNum, sep = ""))
 }
 
-# load words data
-sheet <- read.table("sheet-6.txt", sep = " ")
-sheet <- as.matrix(sheet)
-
-row <- dim(sheet)[1]
-word_list <- list()
-for (i in 1:row) {
-  tmp <- sheet[i,]
-  word_list[[i]] <- tmp[!is.na(tmp)]
+# make word list
+makeWordList <- function(file)
+{
+  # load words data
+  sheet <- read.table(file, sep = " ")
+  sheet <- as.matrix(sheet)
+  
+  row <- dim(sheet)[1]
+  word_list <- list()
+  for (i in 1:row) {
+    tmpList <- sheet[i,]
+    word_list[[i]] <- tmpList[!is.na(tmpList)]
+  }
+  
+  return(word_list)
 }
 
-for (i in 6:6) {
-  #for (i in 6:135) {
-  sheetNum <- i
-  sheet <- read.xlsx("test.xlsm", sheet = sheetNum)[,12:27]
-  write.table(sheet, paste("sheet-", sheetNum, sep = ""))
-}
-
-# load words data
-sheet <- read.table("a", sep = " ")
-sheet <- as.matrix(sheet)
-
-row <- dim(sheet)[1]
-word_list <- list()
-for (i in 1:row) {
-  tmp <- sheet[i,]
-  word_list[[i]] <- tmp[!is.na(tmp)]
-}
+makeWordList("sheet-6.txt")
