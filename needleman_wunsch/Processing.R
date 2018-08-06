0408131552ddac48772a4ce9cb5257bfc16e8000
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-08-06 23:00:38 +0900

    [refactoring] Processing.R

--- needleman_wunsch/Processing.R
+++ needleman_wunsch/Processing.R
@@ -16,14 +16,14 @@ makeWordList <- function(file)
   sheet <- read.table(file, sep = " ")
   sheet <- as.matrix(sheet)
   
-  row <- dim(sheet)[1]
+  nrow <- dim(sheet)[1]
   word_list <- list()
-  for (i in 1:row) {
-    tmpList <- sheet[i,]
+  for (i in 1:nrow) {
+    tmpList <- as.vector(sheet[i,])
     word_list[[i]] <- tmpList[!is.na(tmpList)]
   }
   
   return(word_list)
 }
 
-makeWordList("sheet-6.txt")
+word_list <-  makeWordList("sheet-6.txt")
