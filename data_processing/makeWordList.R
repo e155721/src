e14ea5b8de17e65a7753e71c5c6209c733f2af5e
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-08-26 15:50:28 +0900

    [refactoring] data_processing/makeWordList.R

--- data_processing/makeWordList.R
+++ data_processing/makeWordList.R
@@ -5,11 +5,13 @@ makeWordList <- function(file)
   sheet <- read.table(file, sep = " ")
   sheet <- as.matrix(sheet)
   
+  # get rows number of data sheet
   nrow <- dim(sheet)[1]
+  
   word_list <- list()
   for (i in 1:nrow) {
-    tmpList <- as.vector(sheet[i,])
-    word_list[[i]] <- tmpList[!is.na(tmpList)]
+    tmp_list <- as.vector(sheet[i,])
+    word_list[[i]] <- tmp_list[!is.na(tmp_list)]
   }
   
   return(word_list)
