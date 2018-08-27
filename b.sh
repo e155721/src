e2b0b5dee84da7588001761d9d671435d05901da
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-08-28 00:50:24 +0900

    [rename] b.sh to format_to_needleman.sh

--- b.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-# main
-
-inFile=$1
-outFile=$2
-
-<"$inFile" | sed -e 's/"-1"/NA/g' | sed -e 's/"-9"/NA/g' | sed -e 's/"\."/NA/g' |cut -f2- -d" " | sed -n 3,100p | grep -v '^NA' | grep -v '^\s' >"$outFile"
