57d5ff70b0cc63f4c92167f35dc13aaba2e9d383
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-08-27 16:25:09 +0900

    [update] data_processing/execute_translation_for_all_data_files.R that load translateSymbolArraysIntoVC.R

--- data_processing/execute_translation_for_all_data_files.R
+++ data_processing/execute_translation_for_all_data_files.R
@@ -1,7 +1,8 @@
+source("/Users/e155721/OkazakiLab/src/data_processing/translateSymbolArraysIntoVC.R")
+
 input_path <- "/Users/e155721/OkazakiLab/src/tmp"
 output_path <- "/Users/e155721/OkazakiLab/src/output"
 
 files_list <- paste(input_path, list.files(input_path), sep = "/")
-for (f in files_list) {
+for (f in files_list)
   translateSymbolArraysIntoVC(f, output_path)
-}
