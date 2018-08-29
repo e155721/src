da0baa7c9caff9ae37a375d3bac158639298e731
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-08-29 18:30:22 +0900

    [modify] data_processing/excel_data_abstraction.R

--- data_processing/excel_data_abstraction.R
+++ data_processing/excel_data_abstraction.R
@@ -6,6 +6,10 @@ output_path <- "/Users/e155721/OkazakiLab/src/org_data/"
 
 # get the all sheet names
 sheet_names <- getSheetNames(input_path)
+sheet_names_length <- length(sheet_names)
+for (i in 1:sheet_names_length) {
+  sheet_names[i] <- gsub(" ", "", sheet_names[i])
+}
 
 # output sheets
 for (i in 6:135) {
