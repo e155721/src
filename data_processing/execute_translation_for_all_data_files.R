8069931760e22a5f74a3017dccfc847c69cab123
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-08-29 18:46:27 +0900

    [modify] data_processing/execute_translation_for_all_data_files.R

--- data_processing/execute_translation_for_all_data_files.R
+++ data_processing/execute_translation_for_all_data_files.R
@@ -1,6 +1,6 @@
 source("/Users/e155721/OkazakiLab/src/data_processing/translateSymbolArraysIntoVC.R")
 
-input_path <- "/Users/e155721/OkazakiLab/src/abstraction_array_pattern/org_data/"
+input_path <- "/Users/e155721/OkazakiLab/src/abstraction_array_pattern/preprocessed_data//"
 output_path <- "/Users/e155721/OkazakiLab/src/abstraction_array_pattern/translated_data/"
 
 files_list <- paste(input_path, list.files(input_path), sep = "/")
