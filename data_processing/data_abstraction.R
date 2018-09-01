ccb41ac782b3b08f8406cf1738b907ee2274756d
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-09-01 18:10:36 +0900

    [refactoring] data_processing/data_abstraction.R

--- data_processing/data_abstraction.R
+++ data_processing/data_abstraction.R
@@ -1,17 +1,24 @@
-sheet <- read.table("abstraction_array_pattern/data.txt")
+# array pattern abstraction of all wordsst
+sheet <- read.table("abstraction_array_pattern/all_arrays.txt")
 pattern <- as.matrix(unique(sheet$V2))
-write.table(pattern, "abstraction_array_pattern/array_pattern.txt")
+write.table(pattern, "abstraction_array_pattern/array_pattern_of_all_words.txt")
 
-input_path <- "/Users/e155721/OkazakiLab/src/abstraction_array_pattern-2/pattern_data"
-output_path <- "/Users/e155721/OkazakiLab/src/abstraction_array_pattern-2/test"
+
+# array pattern abstraction by assumed forms
+input_path <- "/Users/e155721/OkazakiLab/src/abstraction_array_pattern/translated_data"
+output_path <- "/Users/e155721/OkazakiLab/src/abstraction_array_pattern/array_pattern_by_asuumed_forms.txt"
 
 files_list <- paste(input_path, list.files(input_path), sep = "/")
 pattern_list <- list()
 i <- 1
 for (f in files_list) {
   s <- read.table(f)
-  pattern_list[[i]] <- as.matrix(s$V1)
+  pattern_list[[i]] <- unique(as.matrix(s$V1))
   i <- i + 1
 }
+pattern_list <- unique(pattern_list)
 
-abstract_pattern_list <- unique(pattern_list)
+len <- length(pattern_list)
+for ( i in 1:len) {
+  write.table(pattern_list[[i]], output_path, append = T, col.names = T, row.names = F)
+}
