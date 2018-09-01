d7b48253d19b35ee2b08630c80eee25ce6f011f4
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-09-01 10:43:44 +0900

    [update] data_processing

--- data_processing/data_abstraction.R
+++ data_processing/data_abstraction.R
@@ -1,4 +1,17 @@
 sheet <- read.table("abstraction_array_pattern/data.txt")
 pattern <- as.matrix(unique(sheet$V2))
-
 write.table(pattern, "abstraction_array_pattern/array_pattern.txt")
+
+input_path <- "/Users/e155721/OkazakiLab/src/abstraction_array_pattern-2/pattern_data"
+output_path <- "/Users/e155721/OkazakiLab/src/abstraction_array_pattern-2/test"
+
+files_list <- paste(input_path, list.files(input_path), sep = "/")
+pattern_list <- list()
+i <- 1
+for (f in files_list) {
+  s <- read.table(f)
+  pattern_list[[i]] <- as.matrix(s$V1)
+  i <- i + 1
+}
+
+abstract_pattern_list <- unique(pattern_list)
--- data_processing/execute_translation_for_all_data_files.R
+++ data_processing/execute_translation_for_all_data_files.R
@@ -1,7 +1,7 @@
 source("/Users/e155721/OkazakiLab/src/data_processing/translateSymbolArraysIntoVC.R")
 
-input_path <- "/Users/e155721/OkazakiLab/src/abstraction_array_pattern/preprocessed_data//"
-output_path <- "/Users/e155721/OkazakiLab/src/abstraction_array_pattern/translated_data/"
+input_path <- "/Users/e155721/OkazakiLab/src/abstraction_array_pattern-2/preprocessed_data"
+output_path <- "/Users/e155721/OkazakiLab/src/abstraction_array_pattern-2/translated_data"
 
 files_list <- paste(input_path, list.files(input_path), sep = "/")
 for (f in files_list)
