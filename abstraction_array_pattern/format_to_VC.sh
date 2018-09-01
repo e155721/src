0f3222baab0e7fa43a3d0733eb2af2a3f60395d1
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-09-01 18:15:09 +0900

    [delete] abstraction_array_pattern

--- abstraction_array_pattern/format_to_VC.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-# This code output data format to use into abstraction of array pattern of vowel, consonant and elision.
-
-inFile=$1
-outFile=$2
-
-<"$inFile" | sed -e 's/"-9"/NA/g' | sed -e 's/"\."/NA/g' |cut -f2- -d" " | sed -n 3,100p | grep -v '^NA' | grep -v '^\s' >"$outFile"
