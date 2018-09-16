fd811a284bfbd86eb1c83f2a5cc5dd0d2a8440c5
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-09-17 00:08:43 +0900

    [add] format.sh

---
+++ format.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+inFile=$1
+outFile=$2
+
+<"$inFile" | sed -e 's/-1/_/g' | sed -e 's/-9/NA/g' | sed -e 's/\./NA/g' | cut -f2- -d" " | sed -n 3,100p | grep '"' | grep -v '^\s' >"$outFile"
