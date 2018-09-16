981a146671f53154af3534756c3cf928c5ed5ca0
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-09-17 01:13:12 +0900

    [fix] format_to_needleman.sh

--- format_to_needleman.sh
+++ format_to_needleman.sh
@@ -3,4 +3,4 @@
 inFile=$1
 outFile=$2
 
-<"$inFile" | sed -e 's/"-1"/NA/g' | sed -e 's/"-9"/NA/g' | sed -e 's/"\."/NA/g' |cut -f2- -d" " | sed -n 3,100p | grep -v '^NA' | grep -v '^\s' >"$outFile"
+<"$inFile" | sed -e 's/"-1"/NA/g' | sed -e 's/"-9"/NA/g' | sed -e 's/"\."/NA/g' | cut -f2- -d" " | sed -n 3,100p | grep -v '^\s' >"$outFile"
