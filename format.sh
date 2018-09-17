d8f8755087055dedc1bfb057542f700bf8fd1c3f
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-09-17 14:47:42 +0900

    [fix] format scripts

--- format.sh
+++ format.sh
@@ -3,4 +3,4 @@
 inFile=$1
 outFile=$2
 
-<"$inFile" | sed -e 's/-1/_/g' | sed -e 's/-9/NA/g' | sed -e 's/\./NA/g' | cut -f2- -d" " | sed -n 3,100p | grep '"' | grep -v '^\s' >"$outFile"
+<"$inFile" | sed -e 's/-1/_/g' | sed -e 's/-9/NA/g' | sed -e 's/\./NA/g' | cut -f2- -d" " | sed -n 3,100p >"$outFile"
