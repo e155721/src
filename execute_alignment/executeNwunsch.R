a7e09d1c990d530142dd99556483405395b039ae
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-09-20 22:24:06 +0900

    [add] executeNwunsch.R

---
+++ execute_alignment/executeNwunsch.R
@@ -0,0 +1,43 @@
+executeNwunsch <- function(input_path = "../Alignment/ex_data/", 
+                           output_path = "../Alignment/align_data/")
+{
+  .myfunc.env <- new.env()
+  sys.source("data_processing/makeWordList.R", envir = .myfunc.env)
+  sys.source("needleman_wunsch/needlemanWunsch.R", envir = .myfunc.env)
+  sys.source("needleman_wunsch/makeScoringMatrix.R", envir = .myfunc.env)
+  attach(.myfunc.env)
+  
+  # make scoring matrix
+  scoring_matrix <- makeScoringMatrix()
+  
+  # input files list
+  name_list <- list.files(input_path)
+  number_of_words <- length(name_list)
+  
+  # input paths and output paths list
+  read_path_list <- paste(input_path, name_list, sep = "")
+  
+  write_base_path <- output_path
+  if (!dir.exists(write_base_path)) {
+    dir.create(write_base_path)
+  }
+  write_path_list <- paste(write_base_path, name_list, ".aln", sep = "")
+  
+  for (i in 1:number_of_words) {
+    word_list <- makeWordList(read_path_list[[i]])
+    word_list_length <- length(word_list)
+    
+    assumed_form <- word_list[[1]]
+    rlt_list <- list()
+    sink(write_path_list[[i]], append = T)
+    for (i in 1:word_list_length) {
+      print(i)
+      seq2 <- word_list[[i]]
+      rlt <- needlemanWunsch(assumed_form, seq2, p = -1, scoring_matrix)
+      #print(toOrg(rlt[["seq1"]], assumed_form[["org"]]))
+      print(rlt[["seq2"]])
+    }
+    sink()
+  }
+}
+executeNwunsch(output_path = "../Alignment/hoge/")
