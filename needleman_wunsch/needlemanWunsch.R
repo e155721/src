0de0ab2da10d409c51f52f751b0b99ece55819b0
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-09-21 00:28:57 +0900

    [add] needlemanWunsch.R

---
+++ needleman_wunsch/needlemanWunsch.R
@@ -0,0 +1,85 @@
+.myfunc.env = new.env()
+sys.source("needleman_wunsch/Functions.R", envir = .myfunc.env)
+attach(.myfunc.env)
+
+needlemanWunsch <- function(seq1, seq2, p = -1, scoringMatrix)
+{
+  # initialize variable
+  seq1 <- append(seq1, NA, after = 0)
+  seq2 <- append(seq2, NA, after = 0)
+  s <- s$new(seq1, seq2, scoringMatrix)
+  
+  # calculate matrix for sequence alignment
+  mat <- makeMatrix(seq1, seq2)
+  mat <- initializeMat(mat, p)
+  
+  rowLen <- length(seq1)
+  colLen <- length(seq2)
+  
+  for (i in 2:rowLen) {
+    for (j in 2: colLen) {
+      mat[i,j, 1] <- D(mat,i,j,p,s)[[1]]
+      mat[i,j, 2] <- D(mat,i,j,p,s)[[2]]
+      j = j + 1
+    }
+    i = i + 1
+  }
+  
+  # trace back
+  score <- gap <- c() 
+  i <- rowLen
+  j <- colLen
+  n <- 1
+  score <- mat[i, j, 1]
+  while (TRUE) {
+    if (i == 1 && j == 1) break
+    gap[n] <- mat[i, j, 2]
+    n <- n + 1
+    
+    trace <- mat[i, j, 2]
+    if (trace == 0) {
+      i <- i - 1
+      j <- j - 1
+    } else if (trace == 1) {
+      i <- i - 1
+    } else if (trace == -1){
+      j <- j - 1
+    }
+  }
+  gap <- rev(gap)
+  
+  # output alignment
+  s1 <- seq1[2:length(seq1)]
+  s2 <- seq2[2:length(seq2)]
+  
+  align1 <- align2 <- c()
+  i <- j <- 1
+  for (t in 1:length(gap)) {
+    if(gap[t] == 0) {
+      align1 <- append(align1, s1[i])
+      align2 <- append(align2, s2[j])
+      i <- i + 1
+      j <- j + 1
+    } else if(gap[t] == 1) {
+      align1 <- append(align1, s1[i])
+      align2<- append(align2, "_")
+      i <- i + 1
+    } else {
+      align1 <- append(align1, "_")
+      align2 <- append(align2, s2[j])
+      j <- j + 1
+    }
+  }
+  
+  if (0) {
+    print(c("seq1: ", align1), quote = F)
+    print(c("seq2: ", align2), quote = F)
+    print(c("score: ", score), quote = F)
+  }
+  align <- list(NA, NA)
+  names(align) <- c("seq1", "seq2")
+  align[["seq1"]] <- align1
+  align[["seq2"]] <- align2
+  
+  return(rlt)
+}
