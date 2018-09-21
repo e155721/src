7576a2468962ac8fec39c1e86fbdedd2bb73bcd5
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-09-21 13:00:14 +0900

    [refactoring] functionalization check_correct_alignment.R

--- execute_alignment/check_correct_alignment.R
+++ execute_alignment/check_correct_alignment.R
@@ -2,45 +2,60 @@
 sys.source("data_processing/makeWordList.R", envir = .myfunc.env)
 sys.source("data_processing/functions_to_convert_symbol.R", envir = .myfunc.env)
 sys.source("needleman_wunsch/needlemanWunsch.R", envir = .myfunc.env)
+sys.source("needleman_wunsch/makeScoringMatrix.R", envir = .myfunc.env)
 attach(.myfunc.env)
 
-# check files list
-check_name_list <- list.files("../Alignment/check_data/")
-read_check_path_list <- paste("../Alignment/check_data/", check_name_list, sep="")
-
-# input files list
-name_list <- list.files("../Alignment/ex_data/")
-number_of_words <- length(name_list)
-
-# input paths and output paths list
-read_path_list <- paste("../Alignment/ex_data/", name_list, sep = "")
-write_path_list <- paste("../Alignment/align_data/", name_list, ".aln", sep = "")
-
-for (i in 1:number_of_words) {
-  word_list <- makeWordList(read_path_list[[i]])
-  check_word_list <- makeWordList(read_check_path_list[[i]])
-  word_list_length <- length(word_list)
+checkCorrectAlignment <- function(input_path = "../Alignment/ex_data/",
+                                  input_correct_path = "../Alignment/check_data/",
+                                  output_compare_path = "../Alignment/compare.txt",
+                                  output_ansrate_path = "../Alignment/ansrate.txt")
+{
+  # make scoring matrix
+  scoring_matrix <- makeScoringMatrix()
   
-  assumed_form <- toCorV(word_list[[1]])
-  rlt_list <- list()
-  sink("../Alignment/compare.txt", append = T)
-  for (k in 1:word_list_length) {
-    seq2 <- toCorV(word_list[[k]])
-    rlt <- needlemanWunsch(assumed_form[["sym"]], seq2[["sym"]], p = -1, scoring_matrix)
-    r <- toOrg(rlt[["seq2"]], seq2[["org"]])
+  # input files list
+  name_list <- list.files(input_path)
+  read_path_list <- paste(input_path, name_list, sep = "")
+  
+  
+  # check files list
+  correct_name_list <- list.files(input_correct_path)
+  read_correct_path_list <- paste(input_correct_path, correct_name_list, sep="")
+  
+  # get number of words
+  number_of_words <- length(name_list)
+  
+  for (i in 1:number_of_words) {
+    word_list <- makeWordList(read_path_list[[i]])
+    check_word_list <- makeWordList(read_correct_path_list[[i]])
+    word_list_length <- length(word_list)
     
-    r <- paste(r, collapse = "")
-    c <- paste(check_word_list[[k]], collapse = "")
-    #print(paste("align: ",r))
-    #print(paste("org:   ", c))
-    if ((r != c) && k != 1) {
-      print(name_list[[i]])
-      print(k)
-      print(paste("align: ", r))
-      print(paste("check: ", c))
-      cat("\n")
-      break
+    assumed_form <- word_list[[1]]
+    rlt_list <- list()
+    sink(output_compare_path, append = T)
+    for (k in 1:word_list_length) {
+      seq2 <- word_list[[k]]
+      align <- needlemanWunsch(assumed_form, seq2, p = -1, scoring_matrix)
+      
+      align <- paste(align[["seq2"]], collapse = "")
+      correct_align <- paste(check_word_list[[k]], collapse = "")
+      #if ((align != correct_align) && (k != 1)) {
+      if (align != correct_align) {
+        print(name_list[[i]])
+        print(k)
+        print(paste("align: ", align))
+        print(paste("check: ", correct_align))
+        cat("\n")
+        break
+      }
     }
+    sink()
+    
+    sink(output_ansrate_path, append = T)
+    #print(paste(k, "/", word_list_length))
+    print(k/word_list_length*100)
+    sink()
   }
-  sink()
 }
+
+checkCorrectAlignment()
