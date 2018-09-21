a99a2d579abbbc628bb79b1a000b425d606082f2
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-09-21 14:44:04 +0900

    [remove] Execution.R

--- needleman_wunsch/Execution.R
+++
@@ -1,9 +0,0 @@
-size <- length(word_list)
-rlt <- list()
-#for (i in 2:73) {
-for (i in 1:10) {
-  #sink("test.aln", append = T)
-  print(paste(i, word_list[[i]]), sep = ": ")
-  needlemanWunsch(as.vector(word_list[[1]]), as.vector(word_list[[i]]), p)
-  #sink()
-}
