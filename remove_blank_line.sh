15cdbd2d3e83364e8e1599dbfb742bf6c05d01b8
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-09-30 18:08:19 +0900

    [add] remove_blank_line.sh

--- remove_blank_line.sh
+++ remove_blank_line.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
 
-inFile=$1
-outFile=$2
-for f (*) cat $f | sed -e 's/NA//g' | sed -e 's/^ /#/' | grep -v \^\# >$f
+outFile=$1
+for f (*) cat $f | sed -e 's/NA//g' | sed -e 's/^ /#/' | grep -v \^\# >$outFile/$f
