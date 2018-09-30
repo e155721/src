58576f05099a40fcc592f937e78df20b89e9b7f4
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-09-30 20:38:20 +0900

    [add] execute_script.R

---
+++ execute_alignment/execute_script.R
@@ -0,0 +1,21 @@
+.myfunc.env <- new.env()
+sys.source("execute_alignment/executeNwunsch.R", envir = .myfunc.env)
+sys.source("execute_alignment/check_correct_alignment.R", envir = .myfunc.env)
+attach(.myfunc.env)
+
+checkCorrectAlignment(input_path = "../Alignment/ex_data/",
+                      input_correct_path = "../Alignment/check_data/",
+                      s1 = 1, 
+                      s2 = 1,
+                      s3 = -1,
+                      s4 = -1, 
+                      s5 = -4,
+                      gap = -2)
+
+executeNwunsch(output_path = "../Alignment/align_data/",
+               s1 = 1, 
+               s2 = 1,
+               s3 = -1,
+               s4 = -1, 
+               s5 = -5,
+               gap = -2)
