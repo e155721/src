3683887b43c679e2c2cc98c69aba67f6b7f8f3b0
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-10-01 20:29:02 +0900

    [refactoring] makeFeatureList.R

--- data_processing/makeFeatureList.R
+++ data_processing/makeFeatureList.R
@@ -2,29 +2,29 @@
 sys.source("data_processing/makeWordList.R", envir = .myfunc.env)
 attach(.myfunc.env)
 
-makeFeatureList <- function()
+makeFeatureList <- function(input_file)
 {
-  a <- read.table("../Feature_Data/ex_feature/01-003首(1-2)")
-  a <- makeWordList("../Feature_Data/ex_feature/01-003首(1-2)")
-  
-  list_length <- length(a)
-  b <- list()
+  x <- makeWordList(input_file)
+  list_length <- length(x)
+  y <- list()
   tmp <- vector()
   
   for (i in 1:list_length) {
-    b[[i]] <- vector()
+    y[[i]] <- vector()
   }
   
   for (i in 1:list_length){
-    vector_length <- length(a[[i]])
+    vector_length <- length(x[[i]])
     
     for (k in 1:vector_length) {
-      tmp <- append(tmp, a[[i]][k], length(tmp))
+      tmp <- append(tmp, x[[i]][k], length(tmp))
       if (k %% 5 == 0) {
-        b[[i]] <- append(b[[i]], sum(tmp), length(b[[i]]))
+        y[[i]] <- append(y[[i]], sum(tmp), length(y[[i]]))
         tmp <- vector()
       }
     }
   }
+  
+  return(y)
 }
 
