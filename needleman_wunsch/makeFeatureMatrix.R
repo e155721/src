e5cc489ef0e65ee28f4706f8de05ebddaa8be957
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-10-04 19:24:55 +0900

    [add] makeFeatureMatrix.R

---
+++ needleman_wunsch/makeFeatureMatrix.R
@@ -0,0 +1,73 @@
+makeFeatureMatrix <- function(s5 = NA)
+{
+  ######
+  # make consonant array
+  consonant <- as.matrix(read.table("symbols/consonant.txt"))
+  consonant_length <- length(consonant)
+  
+  # make vowel array
+  vowel <- as.matrix(read.table("symbols/vowel.txt"))
+  vowel_length <- length(vowel)
+  
+  # get the sum of length of consonant and vowel
+  score_row <- score_col <- consonant_length + vowel_length
+  
+  symbols <- c(consonant, vowel)
+  
+  feature_matrix <- matrix(s5, nrow = score_row, ncol = score_col, 
+                           dimnames = list(symbols, symbols))
+  
+  # consonant feature
+  c_feature <- read.table("../Feature_Data/feature/子音入力数値一覧")
+  k <- dim(c_feature)[[1]]
+  l <- dim(c_feature)[[2]]
+  
+  c <- matrix(NA, k, l, dimnames = list(consonant, c()))
+  c[, 1] <- c_feature$V1
+  c[, 2] <- c_feature$V2
+  c[, 3] <- c_feature$V3
+  c[, 4] <- c_feature$V4
+  c[, 5] <- c_feature$V5
+  
+  c_match <- matrix(NA, k, k)
+  for (i in 1:k) {
+    for (j in 1:k) {
+      match <- 0
+      for (n in 1:5) {
+        if (c[i, n] == c[j, n])
+          match <- match + 1
+      }
+      c_match[i, j] <- match
+    }
+  }
+  
+  # vowel feature
+  v_feature <- read.table("../Feature_Data/feature/母音入力数値一覧")
+  k <- dim(v_feature)[[1]]
+  l <- dim(v_feature)[[2]]
+  
+  v <- matrix(NA, k, l)
+  v[, 1] <- v_feature$V1
+  v[, 2] <- v_feature$V2
+  v[, 3] <- v_feature$V3
+  v[, 4] <- v_feature$V4
+  v[, 5] <- v_feature$V5
+  
+  v_match <- matrix(NA, k, k)
+  for (i in 1:k) {
+    for (j in 1:k) {
+      match <- 0
+      for (n in 1:5) {
+        if (v[i, n] == v[j, n])
+          match <- match + 1    
+      }
+      v_match[i, j] <- match
+    }
+  }
+  
+  ###
+  feature_matrix[1:81, 1:81] <- c_match
+  feature_matrix[82:118, 82:118] <- v_match
+  
+  return(feature_matrix)
+}
