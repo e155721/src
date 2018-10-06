f6486f74023a21af710f0350a96853503e5a509c
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-10-06 18:29:07 +0900

    [add] makeGapComb.R

---
+++ execute_alignment/makeGapComb.R
@@ -0,0 +1,21 @@
+
+
+makeGapComb <- function(min = 1)
+{
+  num_list <- list()
+  tmp <- list()
+  
+  num1 <- c(-1:-min)
+  num2 <- c(-1:-min)
+  
+  k <- 1
+  for (i in 1:min) {
+    for (j in 1:min) {
+      tmp[[1]] <- num1[i]
+      tmp[[2]] <- num2[j]
+      num_list[[k]] <- tmp
+      k <- k + 1
+    }
+  }
+  return(num_list)
+}
