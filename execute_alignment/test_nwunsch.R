428a3babbcc262e31880a6364bfb54c6263e5e76
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-10-06 21:26:00 +0900

    [add] test_nwunsch.R

---
+++ execute_alignment/test_nwunsch.R
@@ -0,0 +1,8 @@
+.myfunc.env = new.env()
+sys.source("needleman_wunsch/functions.R", envir = .myfunc.env)
+sys.source("needleman_wunsch/needlemanWunsch.R", envir = .myfunc.env)
+sys.source("needleman_wunsch/makeScoringMatrix.R", envir = .myfunc.env)
+attach(.myfunc.env)
+
+scoring_matrix <- makeScoringMatrix()
+needlemanWunsch(c("a","i"), c("i", "i", "a"), scoringMatrix = scoring_matrix)
