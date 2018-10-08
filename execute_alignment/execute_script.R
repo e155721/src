4757c9fa309918f15dafab81f7e9f5e6757ce4f4
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-10-08 09:46:25 +0900

    [refactoring] execute_script.R

--- execute_alignment/execute_script.R
+++ execute_alignment/execute_script.R
@@ -4,7 +4,7 @@ sys.source("execute_alignment/checkCorrectAlignment.R", envir = .myfunc.env)
 sys.source("execute_alignment/makeGapComb.R", envir = .myfunc.env)
 attach(.myfunc.env)
 
-num_list <- makeGapComb(1)
+num_list <- makeGapComb(10)
 n <- 1
 for (i in num_list) {
   dir_path <- "../Alignment/"
@@ -16,6 +16,6 @@ for (i in num_list) {
                         output_ansrate_path,
                         s5 = i[[1]],
                         p1 = i[[2]],
-                        p2 = i[[2]])
+                        p2 = i[[2]]+1)
   n <- n + 1
 }
