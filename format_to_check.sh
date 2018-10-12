0ac945f2a03334eea6af7b0f131d31af0dd6fb77
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-10-12 19:18:40 +0900

    [refactoring] format_to scripts

--- format_to_check.sh
+++ format_to_check.sh
@@ -3,4 +3,5 @@
 inFile=$1
 outFile=$2
 
-<"$inFile" | sed -e 's/-1/_/g' | sed -e 's/-9/NA/g' | sed -e 's/\./NA/g' | cut -f2- -d" " | sed -n 3,100p >"$outFile".check
+#<"$inFile" | sed -e 's/-1/_/g' | sed -e 's/-9/NA/g' | sed -e 's/\./NA/g' | cut -f1- -d" " | sed -n 3,100p >"$outFile".check
+<"$inFile" | sed -e 's/-1/_/g' | sed -e 's/-9/NA/g' | sed -e 's/\./NA/g' | cut -f1- -d" " >"$outFile".check
