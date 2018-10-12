e43f3de61d5547c33ee55f1ffef7cf896136d8b3
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-10-12 18:00:20 +0900

    [refactoring] execute_script.R

--- execute_alignment/execute_script.R
+++ execute_alignment/execute_script.R
@@ -4,18 +4,23 @@ sys.source("execute_alignment/checkCorrectAlignment.R", envir = .myfunc.env)
 sys.source("execute_alignment/makeGapComb.R", envir = .myfunc.env)
 attach(.myfunc.env)
 
-num_list <- makeGapComb(10)
-n <- 1
-for (i in num_list) {
-  dir_path <- "../Alignment/"
-  output_compare_path <- paste(dir_path, "compare-", n, ".txt", sep = "")
-  output_ansrate_path <- paste(dir_path, "ansrate-", n, ".txt", sep = "")
-  checkCorrectAlignment(input_path = "../Alignment/ex_data/",
-                        input_correct_path = "../Alignment/check_data/",
-                        output_compare_path,
-                        output_ansrate_path,
-                        s5 = i[[1]],
-                        p1 = i[[2]],
-                        p2 = i[[2]]+1)
-  n <- n + 1
+num_list <- makeGapComb(1)
+for (ext in 4:10) {
+  n <- 1
+  for (i in num_list) {
+    dir_path <- "../Alignment/"
+    dir_path <- output_compare_dir <- paste(dir_path, "ext_open_", ext, "/", sep = "")
+    if (!dir.exists(dir_path))
+      dir.create(dir_path)
+    output_compare_path <- paste(dir_path, "compare-", n, ".txt", sep = "")
+    output_ansrate_path <- paste(dir_path, "ansrate-", n, ".txt", sep = "")
+    checkCorrectAlignment(input_path = "../Alignment/ex_data/",
+                          input_correct_path = "../Alignment/check_data/",
+                          output_compare_path,
+                          output_ansrate_path,
+                          s5 = i[[1]],
+                          p1 = i[[2]],
+                          p2 = i[[2]] + ext)
+    n <- n + 1
+  }
 }
