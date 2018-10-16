9bb21298ac0661af34de591d43666b2e39c201c5
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-10-17 01:46:26 +0900

    [edit] execute_script.R

--- execute_alignment/execute_script.R
+++ execute_alignment/execute_script.R
@@ -24,3 +24,24 @@ for (ext in 4:10) {
     n <- n + 1
   }
 }
+
+
+# constant penalty
+num_list <- makeGapComb(10)
+n <- 1
+for (i in num_list) {
+  dir_path <- "../Alignment/"
+  if (!dir.exists(dir_path))
+    dir.create(dir_path)
+  output_compare_path <- paste(dir_path, "compare-", n, ".txt", sep = "")
+  output_ansrate_path <- paste(dir_path, "ansrate-", n, ".txt", sep = "")
+  checkCorrectAlignment(input_path = "../Alignment/ex_data/",
+                        input_correct_path = "../Alignment/check_data/",
+                        output_compare_path,
+                        output_ansrate_path,
+                        s5 = i[[1]],
+                        p1 = i[[2]],
+                        p2 = i[[2]])
+  n <- n + 1
+}
+
