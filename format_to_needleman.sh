26b40e6ef4b9e5b2b74459b68b8503bb66ba0170
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-10-17 17:08:46 +0900

    [refactoring] removed some commands and added the extension ".dat" to the output filename
    file: format_to_needleman.sh

--- format_to_needleman.sh
+++ format_to_needleman.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
-inFile=$1
-outFile=$2
+# Description: This script formats a file type .org to .input.
+# Arguments: This script received only two arguments that one is input filename and other one is output filename.
 
-#<"$inFile" | sed -e 's/"-1"/NA/g' | sed -e 's/"-9"/NA/g' | sed -e 's/"\."/NA/g' | cut -f1- -d" " | sed -n 3,100p >"$outFile"
-<"$inFile" | sed -e 's/"-1"/NA/g' | sed -e 's/"-9"/NA/g' | sed -e 's/"\."/NA/g' | cut -f1- -d" " >"$outFile"
+input_file=$1
+out_file=$2
+
+#<"$input_file" | sed -e 's/"-1"/NA/g' | sed -e 's/"-9"/NA/g' | sed -e 's/"\."/NA/g' | cut -f1- -d" " | sed -n 3,100p >"$out_file"
+#<"$input_file" | sed -e 's/"-1"/NA/g' | sed -e 's/"-9"/NA/g' | sed -e 's/"\."/NA/g' | cut -f1- -d" " >"$out_file"
+<"$input_file" | sed -e 's/"-1"/NA/g' | cut -f1- -d" " >"$out_file:r".dat
