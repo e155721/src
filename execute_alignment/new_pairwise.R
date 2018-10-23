cb82fcf8820e7ee4d56c809aab759a6c41f90281
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-10-23 18:12:26 +0900

    [modify] changed how to write alignment results
    file: execute_alignment/new_pairwise.R

--- execute_alignment/new_pairwise.R
+++ execute_alignment/new_pairwise.R
@@ -9,18 +9,13 @@ word_list <- word_list[-1]
 seq1 <- word_list[[7]]
 scoring_matrix <- makeFeatureMatrix(-5)
 
-if (file.exists("../Alignment/test.align")) {
-  file.remove("../Alignment/test.align")
-}
-
 wl_row <- length(word_list)
 wl_col <- length(seq1)
 results_matrix <- matrix(NA, wl_row, wl_col)
 i <- 1
 for (seq2 in word_list) {
-  #sink("../Alignment/test.align", append = T)
   results_matrix[i, ] <- needlemanWunsch(seq1, seq2, -5, -5, scoring_matrix)[["seq2"]]
   i <- i + 1
-  #print(seq2)
-  #sink()
 }
+
+write.table(results_matrix, "../Alignment/test.aln")
