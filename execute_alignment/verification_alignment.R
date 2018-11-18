a5deadcc7734e5a566947c4e37c8bac8097e062d
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-11-19 08:50:31 +0900

    [modify] execute the alignment for each score	pair
    file: execute_alignment/verification_alignment.R

--- execute_alignment/verification_alignment.R
+++ execute_alignment/verification_alignment.R
@@ -1,6 +1,7 @@
 .myfunc.env <- new.env()
 sys.source("data_processing/MakeWordList.R", envir = .myfunc.env)
 sys.source("data_processing/RemkSeq.R", envir = .myfunc.env)
+sys.source("execute_alignment/MakeGapComb.R", envir = .myfunc.env)
 sys.source("needleman_wunsch/NeedlemanWunsch.R", envir = .myfunc.env)
 sys.source("needleman_wunsch/MakeScoringMatrix.R", envir = .myfunc.env)
 sys.source("needleman_wunsch/MakeFeatureMatrix.R", envir = .myfunc.env)
@@ -25,72 +26,89 @@ for (i in 1:numOfFiles) {
   filesPath[[i]]["name"] <- inputFilese[[i]]
 }
 
-p1 <- -1
-p2 <- -1
-s5 <- -3
 
-# make scoring matrix
-scoringMatrix <- MakeFeatureMatrix(s5)
 
-ansratePath <- "../Alignment/ansrate-01.txt"
-comparePath <- "../Alignment/compare-01.txt"
 
-# conduct the alignment for each files
-for (f in filesPath) {
+# constant penalty
+numList <- makeGapComb(10, 1)
+n <- 1
+for (num in numList) {
   
-  wordList <- MakeWordList(f["input"])
-  regions <- length(wordList)
-  correct <- MakeWordList(f["correct"])
+  p1 <- num[[1]]
+  p2 <- num[[1]]
+  s5 <- num[[2]]
   
-  # make output file path
-  # output file
-  outputPath <- "../Alignment/"
-  outputPath <- paste(outputPath, gsub("\\..+$", "", f["name"]), ".aln", sep = "")
+  # make scoring matrix
+  scoringMatrix <- MakeFeatureMatrix(s5)
   
-  l <- 2
-  count <- 0
-  for (k in 1:(regions-1)) {
-    # the start of the alignment for each the region pair
-    for (i in l:regions) {
-      correctMat <- RemkSeq(correct[[k]], correct[[i]])
-      align <- NeedlemanWunsch(wordList[[k]], wordList[[i]], p1, p2, scoringMatrix)
-      
-      rltAln <- paste(paste(align$seq1, align$seq2, sep = ""), collapse = "")
-      rltCor <- paste(paste(correctMat[1, ], correctMat[2, ], sep = ""), collapse = "")
-      
-      if (rltAln != rltCor) {
-        sink(comparePath, append = T)
-        cat("\n")
-        print("by The Needleman-Wunsch")
-        print(paste(align$seq1))
-        print(align$seq2)
+  ansratePath <- paste("../Alignment/ansrate-", n, ".txt", sep = "") 
+  comparePath <- paste("../Alignment/compare-", n, ".txt", sep = "")
+  
+  # conduct the alignment for each files
+  for (f in filesPath) {
+    
+    wordList <- MakeWordList(f["input"])
+    regions <- length(wordList)
+    correct <- MakeWordList(f["correct"])
+    
+    # make output file path
+    # output file
+    outputPath <- "../Alignment/"
+    outputPath <- paste(outputPath, gsub("\\..+$", "", f["name"]), ".aln", sep = "")
+    
+    l <- 2
+    count <- 0
+    for (k in 1:(regions-1)) {
+      # the start of the alignment for each the region pair
+      for (i in l:regions) {
+        correctMat <- RemkSeq(correct[[k]], correct[[i]])
+        align <- NeedlemanWunsch(wordList[[k]], wordList[[i]], p1, p2, scoringMatrix)
         
-        cat("\n")
-        print("by The Linguists")
-        print(correctMat[1, ])
-        print(correctMat[2, ])
-        sink()
-      } else {
-        count <- count + 1
+        rltAln <- paste(paste(align$seq1, align$seq2, sep = ""), collapse = "")
+        rltCor <- paste(paste(correctMat[1, ], correctMat[2, ], sep = ""), collapse = "")
+        
+        if (rltAln != rltCor) {
+          sink(comparePath, append = T)
+          cat("\n")
+          print("by The Needleman-Wunsch")
+          print(paste(align$seq1))
+          print(align$seq2)
+          
+          cat("\n")
+          print("by The Linguists")
+          print(correctMat[1, ])
+          print(correctMat[2, ])
+          sink()
+        } else {
+          count <- count + 1
+        }
+        
+        if (0) {
+          sink(outputPath, append = T)
+          cat("\n")
+          print("by The Needleman-Wunsch")
+          print(paste(align$seq1))
+          print(align$seq2)
+          sink()
+        }
       }
-      
-      sink(outputPath, append = T)
-      cat("\n")
-      print("by The Needleman-Wunsch")
-      print(paste(align$seq1))
-      print(align$seq2)
-      sink()
+      # the end of the aligne for each the region pair
+      l <- l + 1
     }
-    # the end of the aligne for each the region pair
-    l <- l + 1
+    
+    if (1) {
+      sink(ansratePath, append = T)
+      matchingRate <- count / sum((regions-1):1) * 100
+      rlt <- paste(f["name"], " ", matchingRate)
+      print(rlt)
+      sink()
+      print(rlt)
+    }
   }
   
-  if (1) {
-    sink(ansratePath, append = T)
-    matchingRate <- count / sum((regions-1):1) * 100
-    rlt <- paste(f["name"], " ", matchingRate)
-    print(rlt)
-    sink()
-    print(rlt)
-  }
+  
+  
+  n <- n + 1
+  
+  
 }
