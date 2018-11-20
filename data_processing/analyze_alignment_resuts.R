0e7d1bae690a44b95dc710e76d6042a5344734f5
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-11-20 11:12:31 +0900

    [modify] refactoring analyze_alignment_resuts.R
    file: data_processing/analyze_alignment_resuts.R

--- data_processing/analyze_alignment_resuts.R
+++ data_processing/analyze_alignment_resuts.R
@@ -1,25 +1,37 @@
 #dirPath <- "../Alignment/ex-10_30/analyze/"
-dirPath <- "../Alignment/ex-11_19/ansrate/"
+#dirPath <- "../Alignment/ex-11_19/ansrate/"
+dirPath <- "../Alignment/ex-11_19-second/ansrate/"
 filesName <- list.files(dirPath)
 filesPath <- paste(dirPath, filesName, sep = "")
 
 # make average list of correct answer rate
-dataList <- c()
+meanVec <- c()
+matchVec <- c()
 i <- 1
 for (f in filesPath) {
+  # read data
   data <- read.table(f)$V3
+  # get the matching words
+  matchVec <- append(matchVec, sum(data == 100), after = length(matchVec))
+  # get the average for each the score pair
   data <- sum(data)/length(data)
-  dataList[[i]] <- data
+  meanVec[i] <- data
   i <- i + 1
 }
+# get the max matching word
+maxMatch <- max(matchVec)
+# get the max matching word files
+maxMatchVec <- filesPath[which(matchVec==maxMatch)]
 
 # make max average list of correct answer rate
-maxAverage <- max(dataList)
+maxAverage <- max(meanVec)
 maxAverageFiles <- list()
+scoreVec <- c()
 i <- 1
 j <- 1
-for (f in dataList) {
+for (f in meanVec) {
   if (f == maxAverage) {
+    scoreVec <- append(scoreVec, i, after = length(scoreVec))
     maxAverageFiles[[j]] <- filesPath[[i]]
     j <- j + 1
   }
@@ -30,21 +42,26 @@ for (f in dataList) {
 h <- list()
 i <- 1
 for (f in maxAverageFiles) {
-  data <- read.table(f)
-  h[[i]] <- hist(data$V2, breaks = seq(0,100,10), main = f)
+  data <- read.table(f)$V3
+  h[[i]] <- hist(data, breaks = seq(0,100,10), main = "",
+                 right = F,
+                 labels = T, 
+                 xaxt = "n",
+                 ylim = c(0,127),
+                 xlab = "Matching Rate (%)",
+                 ylab = "Words Frequency",
+                 bg = "transparent")
+  axis(1, at=seq(0,100,10), pos=-3)
+  abline(v=mean(data), col=2, lwd=3, lty = 2)
+  text(x = mean(data)-20, y = 110, paste("Matching Average =", floor(maxAverage), "%", sep = " "), col = 2)
+  # legend("topleft","Matching Rate Average", col=2, lwd=3, lty = 2)
   i <- i + 1
 }
 
-##
-if (0) {
-  scoreList <- list()
-  i <- 1
-  for (f in maxAverageFiles) {
-    f <- gsub("wrong", "compare", f)
-    f <- gsub("_align", "", f)
-    f <- gsub("txt", "score", f)
-    score <- read.table(f)$V1
-    scoreList[[i]] <- score[1] - score[2]
-    i <- i + 1
-  }
-}
+print(paste("Max Average:", maxAverage, sep = " "))
+print(paste("Max Matching Rate:", (maxMatch/111)*100, sep = " "))
+print(paste("Max Matching Words:", maxMatch, sep = " "))
+
+scorePair <- makeGapComb(10,1)
+print(paste(scorePair[scoreVec]))
+print(paste(maxAverageFiles, maxMatchVec, sep = " "))
