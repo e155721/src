a7309c05be64a51fd2aad407596a3bdc542d9618
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-11-23 21:05:00 +0900

    [modify] refactoring excelDataAbstraction.R
    file: data_processing/excelDataAbstraction.R

--- data_processing/excelDataAbstraction.R
+++ data_processing/excelDataAbstraction.R
@@ -1,6 +1,34 @@
 # load openxlsx
 library(openxlsx)
 
+# FormatData
+FormatData <- function(sheet)
+{
+  # delete the labels and the assumed form
+  sheet <- sheet[-1:-2, ]
+  
+  dim <- dim(sheet)
+  x <- matrix(NA, dim[1], dim[2])
+  
+  for (j in 1:dim[2]) {
+    dots <- sum(sheet[, j] == ".")
+    if (dots != 0) break
+    x[, j] <- as.vector(sheet[, j])
+  }
+  
+  for (i in 1:dim[1]) {
+    if(x[i, 1] == -9) {
+      x[i, ] <- NA
+    } else {
+      x[i, ] <- gsub("-1", "-", x[i, ] )
+    }
+  }
+  
+  return(x)
+}
+
+
+# excelDataAbstraction
 excelDataAbstraction <- function(input_path = "../Data/fix_test_data.xlsm",
                                  output_path = "../Alignment/org_data/")
 {
@@ -16,8 +44,10 @@ excelDataAbstraction <- function(input_path = "../Data/fix_test_data.xlsm",
   }
   
   # output sheets
-  for (i in 6:135) {
-    sheet <- read.xlsx(input_path, sheet = i)[,12:27]
+  # for (i in 6:135) {
+  for (i in 6:6) {
+    sheet <- read.xlsx(input_path, sheet = i)[, 12:27]
+    sheet <- FormatData(sheet)
     write.table(sheet, paste(output_path, sheet_names[i], ".org", sep = ""))
   }
 }
