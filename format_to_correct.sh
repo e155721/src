7e487d05de6d1e45223c6b8c24f95941ffc1b339
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-11-25 18:45:46 +0900

    [modify] fixed format_to_correct.sh

--- format_to_correct.sh
+++ format_to_correct.sh
@@ -9,4 +9,4 @@ out_file=$2
 #<"$input_file" | sed -e 's/-1/_/g' | sed -e 's/-9/NA/g' | sed -e 's/\./NA/g' | cut -f1- -d" " | sed -n 3,100p >"$out_file".check
 #<"$input_file" | sed -e 's/-1/_/g' | sed -e 's/-9/NA/g' | sed -e 's/\./NA/g' | cut -f1- -d" " >"$out_file".check
 #<"$input_file" | sed -e 's/-1/\-/g' | cut -f1- -d" " >"$out_file:r".correct
-<"$input_file" | sed -e 's/-9/NA/g' >"$out_file:r".correct
+<"$input_file" | sed -e 's/"-9"/NA/g' >"$out_file:r".correct
