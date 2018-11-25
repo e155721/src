5560909afa66981322ed7296cdfd6a5a9ca2cf06
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-11-25 23:19:20 +0900

    [modify] refactoring verification_gap_comb.R

--- data_processing/analyze_alignment_resuts.R
+++ data_processing/analyze_alignment_resuts.R
@@ -2,9 +2,7 @@
 sys.source("execute_alignment/MakeGapComb.R", envir = .myfunc.env)
 attach(.myfunc.env)
 
-#dirPath <- "../Alignment/ex-10_30/analyze/"
-dirPath <- "../Alignment/ex-11_19/ansrate/"
-dirPath <- "../Alignment/ex-11_19-second/ansrate/"
+dirPath <- "../Alignment/gap_1/"
 filesName <- list.files(dirPath)
 filesPath <- paste(dirPath, filesName, sep = "")
 
@@ -58,21 +56,18 @@ for (f in maxAverageFiles) {
   axis(1, at=seq(0,100,10), pos=-3)
   abline(v=mean(data), col=2, lwd=3, lty=2)
   text(x = mean(data)-20, y = 110, paste("Matching Rate Average =", round(maxAverage, digits = 3), "%", sep = " "), col = 2)
-  # legend("topleft","Matching Rate Average", col=2, lwd=3, lty = 2)
+  legend("topleft","Matching Rate Average", col=2, lwd=3, lty = 2)
   i <- i + 1
 }
 
 plot(meanVec, type = "l", xlab = "Score Pair", ylab = "Matching Rate Average (%)")
 abline(h=maxAverage, col=2, lwd=3, lty=2)
-#legend(locator(1), legend="Matching Rate Average = 96.909 %", col=2, lwd=3, lty=2, bty = "n")
 legend(locator(1), legend=paste("Matching Rate Average =", round(maxAverage, digits = 3), "%", sep = " "), col=2, lwd=3, lty=2, bty = "n")
-#legend("bottomleft", "Matching Rate Average = 96.909 %", col=2, lwd=3, lty = 2,
-#bty = "n")
 
 print(paste("Max Average:", maxAverage, sep = " "))
 print(paste("Max Matching Rate:", (maxMatch/111)*100, sep = " "))
 print(paste("Max Matching Words:", maxMatch, sep = " "))
 
-scorePair <- makeGapComb(10,1)
+scorePair <- MakeGapComb(10,1)
 print(paste(scorePair[scoreVec]))
 print(paste(maxAverageFiles, maxMatchVec, sep = " "))
