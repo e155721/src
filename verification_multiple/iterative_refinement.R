2f3aae231feed8b595bdee90ace2f94602ac0108
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-12-16 13:11:34 +0900

    [modify] refactored iterative_refinement.R

--- verification_multiple/iterative_refinement.R
+++ verification_multiple/iterative_refinement.R
@@ -5,43 +5,52 @@ sys.source("needleman_wunsch/NeedlemanWunsch.R", envir = .myfunc.env)
 sys.source("data_processing/MakeWordList.R", envir = .myfunc.env)
 attach(.myfunc.env)
 
-file <- "../Alignment/input_data/002.dat"
-file <- "../Alignment/input_data/025.dat"
-
-wordList <- MakeWordList(file)
+# progressive alignmen
+# file <- "../Alignment/input_data/002.dat"
+ file <- "../Alignment/input_data/025.dat"
 
 p <- -3
 smat <- MakeFeatureMatrix(s5 = -10, p)
-paRlt <- ProgressiveAlignment(wordList, -3, smat)
+wordList <- MakeWordList(file)
 
+paRlt <- ProgressiveAlignment(wordList, p, smat)
 pa <- paRlt$multi
+beforeScore <- paRlt$score
 
-before <- paRlt$score
+# iterative refinement
 n <- dim(pa)[1]
 max <- 2 * n * n
 i <- 1
 count <- 0
 while (i <= n) {
-  org <- pa
+  # remove gaps
   seq1 <- pa[i, ]
   seq1 <- gsub("-", NA, seq1)
   seq1 <- seq1[!is.na(seq1)]
   seq1 <- t(as.matrix(seq1))
-  seq2 <- pa[-i, ]
-  rlt <- NeedlemanWunsch(seq1, seq2, -3, -3, smat)
-  after <- rlt$score
-  pa <- rlt$multi
   
-  if (after <= before) {
-    i <- i + 1
-    pa <- org
-  } else {
+  # remove ith sequence
+  seq2 <- pa[-i, ]
+  
+  # new pairwise alignment
+  aln <- NeedlemanWunsch(seq1, seq2, p, p, smat)
+  newPa <- aln$multi
+  afterScore <- aln$score
+  
+  # refine score
+  if (afterScore > beforeScore) {
     count <- count + 1
-    before <- after
-    print(count)
+    print(paste("count:", count))
+    pa <- newPa
+    beforeScore <- afterScore
+  } else {
+    print(paste("line:", i))
+    i <- i + 1
   }
   
+  # exit condition
   if (count == max) {
     break
   }
 }
+
