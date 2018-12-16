eccb02a31183469cf8506c1832c0c6fe0978dc96
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-12-16 17:57:09 +0900

    [modify] refactored VerificationPA.R

--- verification_multiple/VerificationPA.R
+++ verification_multiple/VerificationPA.R
@@ -1,64 +1,25 @@
 .myfunc.env = new.env()
 sys.source("data_processing/MakeWordList.R", envir = .myfunc.env)
 sys.source("verification_multiple/ProgressiveAlignment.R", envir = .myfunc.env)
+sys.source("verification_multiple/MakeCorMat.R", envir = .myfunc.env)
 attach(.myfunc.env)
 
 VerificationPA <- function(inFile, corFile, p, scoringMatrix)
 {
   wordList <- MakeWordList(inFile)
+  lenWordList <- length(wordList$list)
   paRlt <- ProgressiveAlignment(wordList, p, scoringMatrix)
   inputAlign <- paRlt$multi
-  mat <- paRlt$guide
+  gtree <- paRlt$guide
   
-  # make correct matrix
   corWordList <- MakeWordList(corFile)
-  lenCorWordList <- length(corWordList$list)
+  corAlign <- MakeCorMat(corWordList, gtree)
   
-  nrow <- length(corWordList$list)
-  ncol <- length(corWordList$list[[1]])
-  
-  corMat <- c()
-  for (i in 1:nrow) {
-    corMat <- rbind(corMat, corWordList$list[[i]])
-  }
-  
-  j <- 1
-  while (j <= dim(corMat)[2]) {
-    m <- match(corMat[, j], "-")
-    m <- !is.na(m)
-    if (sum(m) == nrow) {
-      corMat <- corMat[, -j]
-    }
-    j <- j + 1
-  }
-  
-  len <- dim(mat)[1]
-  corGuide <- list()
-  for (i in 1:len) {
-    flg <- sum(mat[i, ] < 0)
-    if (flg == 2) {
-      seq1 <- mat[i, 1] * -1
-      seq2 <- mat[i, 2] * -1
-      aln <- rbind(corMat[seq1, ], corMat[seq2, ])
-      corGuide[[i]] <- aln
-    } 
-    else if(flg == 1) {
-      clt <- mat[i, 2]
-      seq2 <- mat[i, 1] * -1
-      aln <- rbind(corGuide[[clt]], corMat[seq2, ])
-      corGuide[[i]] <- aln
-    } else {
-      clt1 <- mat[i, 1]
-      clt2 <- mat[i, 2]
-      aln <- rbind(corGuide[[clt1]], corGuide[[clt2]])
-      corGuide[[i]] <- aln
-    }
-  }
+  nrow <- dim(corAlign)[1]
   
   # calculate matching rate
-  corAlign <- tail(corGuide, n = 1)[[1]]
   count <- 0
-  for (i in 1:lenCorWordList) {
+  for (i in 1:nrow) {
     input <- paste(inputAlign[i, ], collapse = "")
     correct <- paste(corAlign[i, ], collapse = "")
     if (input == correct) {
