99b432311b759364e6b41181fc4f627a96ac58c1
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-12-17 00:42:34 +0900

    [modify] functionalized align_for_each.R

--- verification_multiple/align_for_each.R
+++ verification_multiple/align_for_each.R
@@ -6,22 +6,28 @@ sys.source("verification_multiple/VerificationPA.R", envir = .myfunc.env)
 sys.source("verification_multiple/VerificationIR.R", envir = .myfunc.env)
 attach(.myfunc.env)
 
-# make scoring matrix and gap penalty
-scoringMatrix <- MakeFeatureMatrix(-10, -3)
-p <- -3
-
-# get the all of files path
-filesPath <- GetFilesPath(inputDir = "../Alignment/input_data/",
-                          correctDir = "../Alignment/correct_data/")
-
-# alignment for each
-for (f in filesPath) {
-  # matchingRate <- VerificationPA(f[["input"]], f[["correct"]], p, scoringMatrix)
-  matchingRate <- VerificationIR(f[["input"]], f[["correct"]], p, scoringMatrix)
-  print(paste(basename(f[["input"]]), matchingRate, sep = ": "))
+verif <- function(method, output = "multi_test.txt")
+{
+  # make scoring matrix and gap penalty
+  scoringMatrix <- MakeFeatureMatrix(-10, -3)
+  p <- -3
   
-  # sink("multi_test.txt", append = T)
-  sink("IR_result.txt", append = T)
-  print(paste(f[["name"]], matchingRate, sep = " "), quote = F)
-  sink()
+  # get the all of files path
+  filesPath <- GetFilesPath(inputDir = "../Alignment/input_data/",
+                            correctDir = "../Alignment/correct_data/")
+  
+  # alignment for each
+  for (f in filesPath) {
+    switch (method,
+            "pa" = matchingRate <- VerificationPA(f[["input"]], f[["correct"]], p, scoringMatrix),
+            "ir" = matchingRate <- VerificationIR(f[["input"]], f[["correct"]], p, scoringMatrix)
+    )
+    # matchingRate <- VerificationPA(f[["input"]], f[["correct"]], p, scoringMatrix)
+    # matchingRate <- VerificationIR(f[["input"]], f[["correct"]], p, scoringMatrix)
+    print(paste(basename(f[["input"]]), matchingRate, sep = ": "))
+    
+    sink(output, append = T)
+    print(paste(f[["name"]], matchingRate, sep = " "), quote = F)
+    sink()
+  }
 }
