6de8ae98f44fb9b0f01d111d648961cbd4f24e1e
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-12-17 20:17:30 +0900

    [modify] refactored test_nwunsch.R

--- execute_alignment/test_nwunsch.R
+++ execute_alignment/test_nwunsch.R
@@ -5,11 +5,10 @@ sys.source("needleman_wunsch/MakeFeatureMatrix.R", envir = .myfunc.env)
 sys.source("data_processing/MakeWordList.R", envir = .myfunc.env)
 attach(.myfunc.env)
 
-wordList <- MakeWordList("../Alignment/input_data/002.dat")
+wordList <- MakeWordList("../Alignment/input_data/011.dat")
 seq1 <- wordList[[1]]
-seq2 <- wordList[[10]]
+seq2 <- wordList[[2]]
 
 scoringMatrix <- MakeFeatureMatrix(-10, -3)
-p1 <- p2 <- -3
-NeedlemanWunsch(seq1, seq2, s = scoringMatrix, p1, p2)
-aln <- NeedlemanWunsch(seq1, seq2, s = scoringMatrix, p1, p2)
+p <- -3
+NeedlemanWunsch(seq1, seq2, s = scoringMatrix, p, p)
