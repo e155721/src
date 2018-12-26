42f7fad1f3bdec4f94bc3e6c14fbf5884411d2de
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-12-26 14:27:47 +0900

    [modify] fixed the sequence to remove line that is all gap
    file: verification_multiple/DelGap.R

--- verification_multiple/DelGap.R
+++ verification_multiple/DelGap.R
@@ -1,12 +1,20 @@
 DelGap <- function(x)
 {
+  d.vec <- c()
   j <- 1
   while (j <= dim(x)[2]) {
     g.vec <- grep("-", x[, j])
     if (dim(x)[1] == length(g.vec)) {
-      x <- x[, -j]
+      d.vec <- append(d.vec, j)
     }
     j <- j + 1
   }
+
+  d.vec <- rev(d.vec)
+  for (j in d.vec) {
+    x <- x[, -j]
+  }
+  
   return(x)
 }
+
