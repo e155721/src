0faa1cca2398c18fe4a442cef4a0b869aa27d810
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-12-28 17:44:35 +0900

    [modify] fixed the default argument
    file: test/test_msa.R

--- test/test_msa.R
+++ test/test_msa.R
@@ -1,25 +1,23 @@
-.myfunc.env = new.env()
-sys.source("data_processing/MakeFeatureList.R", envir = .myfunc.env)
-sys.source("data_processing/MakeWordList.R", envir = .myfunc.env)
-sys.source("verification_multiple/ProgressiveAlignment.R", envir = .myfunc.env)
-sys.source("verification_multiple/RemoveFirst.R", envir = .myfunc.env)
-sys.source("verification_multiple/BestFirst.R", envir = .myfunc.env)
-sys.source("verification_multiple/Random.R", envir = .myfunc.env)
-sys.source("verification_multiple/check_nwunsch.R", envir = .myfunc.env)
-attach(.myfunc.env)
+source("data_processing/MakeFeatureList.R")
+source("data_processing/MakeWordList.R")
+source("verification_multiple/ProgressiveAlignment.R")
+source("verification_multiple/RemoveFirst.R")
+source("verification_multiple/BestFirst.R")
+source("verification_multiple/Random.R")
+source("verification_multiple/check_nwunsch.R")
 
-test <- function(file)
+test <- function(file, method)
 {
   wordList <- MakeWordList(file)
   p <- -3
   s <- MakeFeatureMatrix(-10, p)
-  
+
   switch(method,
          "pa" = msa <- ProgressiveAlignment(wordList, p, s),
          "rf" = msa <- RemoveFirst(wordList, p, s),
          "bf" = msa <- BestFirst(wordList, p, s),
          "rd" = msa <- Random(wordList, p, s)
   )
-  
+
   return(msa)
 }
