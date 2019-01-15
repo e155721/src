ce9dc3196ad04af70b76e3e4a09de8408a738818
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-01-15 23:20:18 +0900

    [modify] refactored the script
    file: data_processing/plot_time.R

--- data_processing/plot_time.R
+++ data_processing/plot_time.R
@@ -1,29 +1,28 @@
 source("verification_multiple/align_for_each.R")
-words.vec <- seq(10,100,10)
+words.vec <- seq(10,110,10)
 
-dir <- "../Alignment/ex-msa/ex-01_13/ex-time/rf_time/"
-dir <- "../Alignment/ex-msa/ex-01_13/ex-time/bf_time/"
-dir <- "../Alignment/ex-msa/ex-01_13/ex-time/rd_time/"
+dir_rf <- "../Alignment/ex-msa/ex-01_13/ex-time/rf_time/"
+dir_bf <- "../Alignment/ex-msa/ex-01_13/ex-time/bf_time/"
+dir_rd <- "../Alignment/ex-msa/ex-01_13/ex-time/rd_time/"
 
-files <- GetFilesPath(dir)
-time.vec <- c()
-i <- 0
-for (f in files) {
-  i <- i + 1
-  time <- read.table(f[["input"]])
-  time.vec[i] <- sum(time$V2)
+GetTime <- function(dir, words.vec)
+{
+  files <- GetFilesPath(dir)
+  time.vec <- c()
+  i <- 0
+  for (f in files) {
+    time <- read.table(f[["input"]])
+    time.vec <- append(time.vec, sum(time$V2))
+  }
+  
+  mat <- cbind(words.vec, time.vec)
+  return(mat)
 }
 
-mat <- cbind(words.vec, rf.time)
-mat <- cbind(mat, bf.time)
-mat <- cbind(mat, rd.time)
+rf.time <- GetTime(dir_rf, words.vec)
+bf.time <- GetTime(dir_bf, words.vec)
+rd.time <- GetTime(dir_rd, words.vec)
 
-write.table(mat, "time.txt")
-
-write.table(cbind(words.vec, rf.time), "rf.time")
-write.table(cbind(words.vec, bf.time), "bf.time")
-write.table(cbind(words.vec, rd.time), "rd.time")
-
-
-plot(time.vec, type = "l")
-
+write.table(rf.time, "rf.time")
+write.table(bf.time, "bf.time")
+write.table(rd.time, "rd.time")
