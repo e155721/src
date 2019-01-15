8ecc15935af89a4535094c3d42f252f1453a318f
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-01-15 11:37:39 +0900

    [modify] updated the script
    file: data_processing/plot_time.R

--- data_processing/plot_time.R
+++ data_processing/plot_time.R
@@ -1,7 +1,10 @@
 source("verification_multiple/align_for_each.R")
 words.vec <- seq(10,100,10)
 
-dir <- "../Alignment/bf_time/"
+dir <- "../Alignment/ex-msa/ex-01_13/ex-time/rf_time/"
+dir <- "../Alignment/ex-msa/ex-01_13/ex-time/bf_time/"
+dir <- "../Alignment/ex-msa/ex-01_13/ex-time/rd_time/"
+
 files <- GetFilesPath(dir)
 time.vec <- c()
 i <- 0
@@ -10,3 +13,17 @@ for (f in files) {
   time <- read.table(f[["input"]])
   time.vec[i] <- sum(time$V2)
 }
+
+mat <- cbind(words.vec, rf.time)
+mat <- cbind(mat, bf.time)
+mat <- cbind(mat, rd.time)
+
+write.table(mat, "time.txt")
+
+write.table(cbind(words.vec, rf.time), "rf.time")
+write.table(cbind(words.vec, bf.time), "bf.time")
+write.table(cbind(words.vec, rd.time), "rd.time")
+
+
+plot(time.vec, type = "l")
+
