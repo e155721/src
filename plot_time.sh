29468e14d7c811be608079d94b6309e85d7691c5
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-01-16 00:07:01 +0900

    [modify] refactored the script
    file: plot_time.sh

--- plot_time.sh
+++ plot_time.sh
@@ -9,5 +9,8 @@ set xrange [10:110]
 set xlabel 'Words'
 set ylabel 'Time (s)'
 
-plot "rf.time" u 2:3 w lp pt 7 title 'Remove First', "bf.time" u 2:3 w lp pt 9 title 'Best First', "rd.time" u 2:3 w lp pt 5 title 'Random'
+plot "rf.time" u 2:3 w lp pt 5 title 'Remove First',\
+ "bf.time" u 2:3 w lp pt 7 title 'Best First',\
+ "bf.time-par" u 2:3 w lp pt 9 title 'Best First (Parallelize)',\
+ "rd.time" u 2:3 w lp pt 13 title 'Random'
 EOF
