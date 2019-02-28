bda2296c16a08e4599f6447cabef30219df877a5
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-01 03:53:41 +0900

    [modify] refactored the two scripts
    modified:   gpu2org.sh
    modified:   org2gpu.sh

--- gpu2org.sh
+++ gpu2org.sh
@@ -1,8 +1,18 @@
 #!/bin/zsh
 
-gpu_name=($(</Users/e155721/OkazakiLab/Experiment/Alignment/gpu_name))
-org_name=($(</Users/e155721/OkazakiLab/Experiment/Alignment/org_name))
+gpu_name=($(</Users/e155721/OkazakiLab/Experiment/src/name_table/gpu_name))
+org_name=($(</Users/e155721/OkazakiLab/Experiment/src/name_table/org_name))
 
-i=1; for f in $gpu_name; do mv $f.org $org_name[$i].org; i=$((i+1)); done
-i=1; for f in $gpu_name; do mv $f.dat $org_name[$i].dat; i=$((i+1)); done
-i=1; for f in $gpu_name; do mv $f.correct $org_name[$i].correct; i=$((i+1)); done
+ext=$1
+
+if [ -z "$ext" ]; then
+    echo "ERROR!"
+    exit
+fi
+
+i=1
+for f in $gpu_name
+do
+    mv "$f"."$ext" "$org_name[$i]"."$ext"
+    i="$((i+1))"
+done
