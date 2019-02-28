226e27b2ecdd191fbef849a56d8b4baa36b29fcf
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-01 03:23:43 +0900

    [modify] unified the two functions of format_to_correct.sh and format_to_needleman_.sh to prepare_data.sh
    deleted:    format_to_correct.sh
    deleted:    format_to_needleman.sh
    modified:   prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -5,9 +5,14 @@
 
 # remove files that there have the word of original form
 
-pwd
 for f in *.org
 do
     l=($(grep -v "\-9" $f | wc -l))
     [ $l[1] -le 2 ] && rm $f
+done
+
+for f in *.org
+do
+    <"$f" | sed -e 's/"-"/NA/g' | sed -e 's/"-9"/NA/g' >"$f:r".input
+    <"$f" | sed -e 's/"-9"/NA/g' >"$f:r".correct
 done
