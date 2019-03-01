062851a99f8c01af65c9b23d5bfe793c8d0f0487
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-01 12:23:25 +0900

    [modify] unified the function of MakeFeatureList to GetFeaturesScore
    modified:   data_processing/GetFeaturesScore.R
    deleted:    data_processing/MakeFeatureList.R

--- data_processing/GetFeaturesScore.R
+++ data_processing/GetFeaturesScore.R
@@ -1,14 +1,39 @@
-.myfunc.env <- new.env()
-source("data_processing/MakeFeatureList.R")
+source("data_processing/MakeWordList.R")
 
+# MakeFeatureList
+MakeFeatureList <- function(input_file)
+{
+  x <- MakeWordList(input_file)
+  list_length <- length(x)
+  y <- list()
+  tmp <- vector()
+  
+  for (i in 1:list_length) {
+    y[[i]] <- vector()
+  }
+  
+  for (i in 1:list_length){
+    vector_length <- length(x[[i]])
+    
+    for (k in 1:vector_length) {
+      tmp <- append(tmp, x[[i]][k], length(tmp))
+      if (k %% 5 == 0) {
+        y[[i]] <- append(y[[i]], sum(tmp), length(y[[i]]))
+        tmp <- vector()
+      }
+    }
+  }
+  
+  return(y)
+}
 
 # This function return a value that it type is vector
 GetFeaturesScore <- function(consonant_file = "../Feature_Data/feature/母音入力数値一覧",
                              vowel_file = "../Feature_Data/feature/子音入力数値一覧")
   
 {
-  consonant_feature <- makeFeatureList(consonant_file)
-  vowel_feature <- makeFeatureList(vowel_file)
+  consonant_feature <- MakeFeatureList(consonant_file)
+  vowel_feature <- MakeFeatureList(vowel_file)
   score_feature <- append(consonant_feature, vowel_feature)
   
   consonant <- paste(read.table("../src/symbols/consonant.txt")$V1, collapse = " ")
