4b3490b1e4229ed61c1fc0796f3e15b3eefb4728
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-01 13:12:51 +0900

    [modify] refactored the script
    deleted:    data_processing/ExcelFeatureExtraction.R
    new file:   data_processing/ExtractFeaturesValues.R

---
+++ data_processing/ExtractFeaturesValues.R
@@ -0,0 +1,27 @@
+source("data_processing/MakeWordList.R")
+
+ExtractFeaturesValues <- function(input_path = "../Data/test_data.xlsm",
+                                    output_path = "features/")
+{
+  if (!dir.exists(output_path)) {
+    dir.create(output_path)
+  }
+  
+  # output vowel sheet
+  i <- 2
+  vName <- "vowels_values"
+  sheet <- read.xlsx(input_path, sheet = i)[ 1:37, 3:7]
+  write.table(sheet, 
+              paste(output_path, vName, sep = "/"),
+              row.names = F,
+              col.names = F)
+  
+  # output consonant sheet
+  i <- 4
+  cName <- "consonants_values"
+  sheet <- read.xlsx(input_path, sheet = i)[1:81, 3:7]
+  write.table(sheet, 
+              paste(output_path, cName, sep = "/"),
+              row.names = F,
+              col.names = F)
+}
