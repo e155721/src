f8e57785b28dd507e88ee6df2b011384d3557065
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-02 17:57:18 +0900

    [modify] refactored align_for_each.R
    modified:   verification_multiple/align_for_each.R

--- verification_multiple/align_for_each.R
+++ verification_multiple/align_for_each.R
@@ -1,36 +1,13 @@
-
 source("needleman_wunsch/MakeFeatureMatrix.R")
-source("verification/GetFilesPath.R")
-source("verification_multiple/ProgressiveAlignment.R")
-source("verification_multiple/VerificationPA.R")
-source("verification_multiple/VerificationRF.R")
-source("verification_multiple/VerificationBF.R")
-source("verification_multiple/VerificationRD.R")
-
+source("data_processing//GetFilesPath.R")
+source("verification_multiple/VerificationIR.R")
 
 library(foreach)
 library(doParallel)
 registerDoParallel(detectCores())
 
-MPA <- function(f, method, output, p, s, words)
-{
-  switch (method,
-          "pa" = matchingRate <- VerificationPA(f[["input"]], f[["correct"]], p, s, words),
-          "rf" = matchingRate <- VerificationRF(f[["input"]], f[["correct"]], p, s, words),
-          "bf" = matchingRate <- VerificationBF(f[["input"]], f[["correct"]], p, s, words),
-          "rd" = matchingRate <- VerificationRD(f[["input"]], f[["correct"]], p, s, words)
-  )
-  
-  sink(output, append = T)
-  print(paste(f[["name"]], matchingRate), quote = F)
-  sink()
-}
-
 verif <- function(method, output = "multi_test.txt", p = -3, words = NA)
 {
-  # make scoring matrix and gap penalty
-  s <- MakeFeatureMatrix(-10, p)
-  
   # get the all of files path
   filesPath <- GetFilesPath(inputDir = "../Alignment/input_data/",
                             correctDir = "../Alignment/correct_data/")
@@ -42,7 +19,11 @@ verif <- function(method, output = "multi_test.txt", p = -3, words = NA)
   
   # alignment for each
   msa.list <- foreach (f = filesPath) %dopar% {
-    MPA(f, method, output, p, s, words)
+    matchingRate <- VerificationIR(method, f[["input"]], f[["correct"]], p, words)
+    sink(output, append = T)
+    print(paste(f[["name"]], matchingRate), quote = F)
+    sink()
   }
   
+  return(0)
 }
