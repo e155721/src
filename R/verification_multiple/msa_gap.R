c240a97fb24d371e60561c449abc165c90496cea
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-03 23:08:32 +0900

    [modify] refactored verification_gap.R
    modified:   verification/verification_gap.R

--- R/verification_multiple/msa_gap.R
+++ R/verification_multiple/msa_gap.R
@@ -3,6 +3,6 @@ source("verification_multiple/VerifForEeach.R")
 methods <- c("rf", "bf", "rd")
 for (method in methods) {
   for (p in -3:-5) {
-    verif(method, output = paste("rf_p", p, ".rlt", sep = ""), p = p)
+    verif(method, output = paste(method, p, ".rlt", sep = ""), p = p)
   }
 }
