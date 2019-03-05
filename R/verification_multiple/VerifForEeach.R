ed3675c3afddbf28aaea9e13e320afa783e2d0d2
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-05 16:31:18 +0900

    [modify] modified the process of the mathching rate output in VerifForEach.R
    modified:   verification_multiple/VerifForEeach.R

--- R/verification_multiple/VerifForEeach.R
+++ R/verification_multiple/VerifForEeach.R
@@ -1,5 +1,5 @@
 source("needleman_wunsch/MakeFeatureMatrix.R")
-source("data_processing/GetFilesPath.R")
+source("data_processing/GetPathList.R")
 source("verification_multiple/VerificationIR.R")
 
 library(foreach)
@@ -9,7 +9,7 @@ registerDoParallel(detectCores())
 verif <- function(method, output = "multi_test.txt", p = -3, words = NA)
 {
   # get the all of files path
-  filesPath <- GetFilesPath(inputDir = "../../Alignment/input_data/",
+  filesPath <- GetPathList(inputDir = "../../Alignment/input_data/",
                             correctDir = "../../Alignment/correct_data/")
   
   # decide the number of words
@@ -21,7 +21,7 @@ verif <- function(method, output = "multi_test.txt", p = -3, words = NA)
   msa.list <- foreach (f = filesPath) %dopar% {
     matchingRate <- VerificationIR(method, f[["input"]], f[["correct"]], p, words)
     sink(output, append = T)
-    print(paste(f[["name"]], matchingRate), quote = F)
+    print(paste(gsub("\\..*$", "", f[["name"]]), matchingRate), quote = F)
     sink()
   }
   
