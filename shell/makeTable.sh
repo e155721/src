09389ddb7c9fd61e2008aaef97c1668997c627dd
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-08 01:16:10 +0900

    [add] added makeTable.sh

---
+++ shell/makeTable.sh
@@ -0,0 +1,38 @@
+#!/bin/zsh
+
+
+files=(*.tex)
+
+dir="table"
+mkdir "$dir"
+cd "$dir"
+
+for f in $files
+do
+
+    cat >"$f" <<EOF
+\documentclass[a4j]{jarticle}
+\usepackage{longtable}
+
+\author{}
+\date{}
+\title{}
+
+% "$f:r"
+\begin{document}
+\pagestyle{empty}
+
+\input{../"$f"}
+
+\end{document}
+EOF
+
+    ctex
+    ctex
+    rm "$f"
+
+done
+
+rtex
+
+/Users/e155721/OkazakiLab/Experiment/src/shell/gpu2org.sh pdf
