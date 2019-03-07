048bda328f0b5f718b5efc87c85b973f046c60a5
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-08 03:52:26 +0900

    [add/modify] added IRTime.R and refactored msa_time.R
    new file:   verification_multiple/IRTime.R
    modified:   verification_multiple/msa_time.R

--- R/verification_multiple/msa_time.R
+++ R/verification_multiple/msa_time.R
@@ -1,9 +1,17 @@
-source("verification_multiple/VerifForEach.R")
+source("verification_multiple/IRTime.R")
+source("data_processing/GetPathList.R")
 
-methods <- c("rf", "bf", "rd")
 wordNum <- seq(10,110,10)
-for (method in methods) {
+
+for (method in c("rf", "bf", "rd")) {
   for (words in wordNum) {
-    verif(method, output = paste(method, "_p", -3, ".rlt", sep = ""), p = -3, words = words)
+    # decide the number of words
+    filesPath <- GetPathList()
+    filesPath <- filesPath[1:words]
+    
+    # alignment for each
+    for (f in filesPath) {
+      IRTime(method, f[["input"]], words)
+    }
   }
 }
