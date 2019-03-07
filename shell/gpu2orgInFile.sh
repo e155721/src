870538ae24e32177d89b6c78d1a2ea6462518a22
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-08 00:23:06 +0900

    [update] updated the two of scripts which gpu2orgInFile.sh and org2gpuInFile.sh
    modified:   gpu2orgInFile.sh
    modified:   org2gpuInFile.sh

--- shell/gpu2orgInFile.sh
+++ shell/gpu2orgInFile.sh
@@ -3,12 +3,19 @@
 gpu_name=($(</Users/e155721/OkazakiLab/Experiment/src/data/name_table/gpu_name))
 org_name=($(</Users/e155721/OkazakiLab/Experiment/src/data/name_table/org_name))
 
+ext=$1
+
+if [ -z "$ext" ]; then
+    echo "ERROR!"
+    exit
+fi
+
 for f in *
 do
     i=1
     for gpu in $gpu_name
     do
-        sed -i.tmp "s/$gpu$/$org_name[$i]/" "$f"
+        sed -i.tmp "s/$gpu.$ext/$org_name[$i]/" "$f"
         i=$((i+1))
     done
 done
