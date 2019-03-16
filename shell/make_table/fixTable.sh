2100300f0238bd49a197c47606db5ca7152acc0c
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-17 07:26:08 +0900

    [update] updated fixTable.tex
    It inputs automatically the table and columns names.

--- shell/make_table/fixTable.sh
+++ shell/make_table/fixTable.sh
@@ -6,11 +6,23 @@ for f in $files
 do
     sed -i.tmp 's/^.*¥ &//' "$f"
 
-    sed -i.tmp 's/\\begin{table}\[ht\]/#/' "$f"
-    sed -i.tmp "s/#/% $f/" "$f"
+    # remove table
+    sed -i.tmp 's/\\begin{table}\[ht\]//' "$f"
     sed -i.tmp 's/\\end{table}//' "$f"
 
+    # change tabular to longtable
     sed -i.tmp 's/tabular/longtable/' "$f"
+
+    line=$(<"$f" grep -n "longtable" | head -n1 | cut -f1 -d":")
+    line=$((line+3))
+    cat <<EOF | sed -i.tmp "${line}r /dev/stdin" "$f"
+    % \multicolumn{1}{c}{"$f"} \\\\
+    % \hline
+    % & \multicolumn{1}{c}{Region Name}
+    % & \multicolumn{1}{c|}{Pairwise Alignment}
+    % & \multicolumn{1}{c}{Linguists}\\\\
+    % \hline
+EOF
 
     sed -i.tmp 's/\-/$\-$/g' "$f"
 done
