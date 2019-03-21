3920ffb0f01a23f9461c9e2e6588093ad97d31fa
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-21 17:32:16 +0900

    [modify] refactored test_nwunsch.R
    modified:   R/test/test_nwunsch.R

--- R/test/test_nwunsch.R
+++ R/test/test_nwunsch.R
@@ -1,13 +1,9 @@
-
 source("needleman_wunsch/functions.R")
 source("needleman_wunsch/NeedlemanWunsch.R")
 source("needleman_wunsch/MakeFeatureMatrix.R")
 source("data_processing/MakeWordList.R")
 
-
-wordList <- MakeWordList("../../Alignment/input_data/005.dat")
-wordList <- MakeWordList("../../Alignment/input_data/018.dat")
-wordList <- MakeWordList("../../Alignment/input_data/001.dat")
+wordList <- MakeWordList("../../Alignment/input_data/002.input")
 seq1 <- wordList[[1]]
 seq2 <- wordList[[2]]
 seq2 <- wordList[[3]]
@@ -16,11 +12,11 @@ scoringMatrix <- MakeFeatureMatrix(-10, -3)
 p <- -3
 
 wl.len <- length(wordList)
-seq1 <- NeedlemanWunsch(wordList[[1]], wordList[[2]], s = scoringMatrix, p, p)
-NeedlemanWunsch(seq1$multi, wordList[[3]], s = scoringMatrix, p, p)
+seq1 <- NeedlemanWunsch(wordList[[1]], wordList[[2]], s = scoringMatrix)
+NeedlemanWunsch(seq1$multi, wordList[[3]], s = scoringMatrix)
 
 for (i in 3:wl.len) {
-  seq1 <- NeedlemanWunsch(seq1$multi, wordList[[i]], s = scoringMatrix, p, p)
+  seq1 <- NeedlemanWunsch(seq1$multi, wordList[[i]], s = scoringMatrix)
   print(seq1$multi)
 }
 print(seq1$score)
