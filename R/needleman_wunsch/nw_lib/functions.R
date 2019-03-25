c8407df09c7496c721956f00a87aaec582d24276
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-25 12:08:04 +0900

    [fix] fixed the process to make the library path.
    It can get the path in any environment by this fix.
    
    modified:   needleman_wunsch/nw_lib/functions.R

--- R/needleman_wunsch/nw_lib/functions.R
+++ R/needleman_wunsch/nw_lib/functions.R
@@ -1,4 +1,6 @@
-libPath <- "/Users/e155721/OkazakiLab/Experiment/src/R/needleman_wunsch/nw_lib/"
+libPath <- paste(getwd(), "needleman_wunsch/nw_lib/", sep = "/")
+
+#  "/Users/e155721/OkazakiLab/Experiment/src/R/needleman_wunsch/nw_lib/"
 
 source(paste(libPath, "MakeMatrix.R", sep = ""))
 source((paste(libPath, "InitializeMat.R", sep = "")))
