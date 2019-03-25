2e85bd8a45fac72c2cc4b96813e6af5b0061e59c
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-25 12:00:39 +0900

    [update] updated the verification script for RD
    It makes the directories for each gap penalty.
    
    modified:   verification_multiple/msa_rd.R

--- R/verification_multiple/msa_rd.R
+++ R/verification_multiple/msa_rd.R
@@ -1,10 +1,19 @@
-source("verification_multiple/align_for_each.R")
+source("verification_multiple/for_acc/VerifForEach.R")
 
+if (0) {
+  for (i in 1:100) {
+    dir <- paste("rd-etr_", i, sep = "")
+    dir.create(dir)
+    for (p in -3:-5) {
+      verif(method = "rd", output = paste(dir, "/", "rd_p", p, ".rlt", sep = ""), p = p)
+    }
+  }
+}
 
-for (i in 1:10) {
-  dir <- paste("rd-", i, sep = "")
+for (p in -3:-5) {
+  dir <- paste("../../Alignment/rd-p_", i, sep = "")
   dir.create(dir)
-  for (p in -3:-5) {
-    verif(method = "rd", output = paste(dir, "/", "rd_p", p, ".dat", sep = ""), p = p)
+  for (i in 1:100) {
+    verif(method = "rd", output = paste(dir, "/", "rd-etr_", i, ".rlt", sep = ""), p = p)
   }
 }
