4a369a50f692042286c78c4ce316005080e05908
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-25 12:55:24 +0900

    [fix] fixed some errors in msa_rd.R
    modified:   R/verification_multiple/msa_rd.R

--- R/verification_multiple/msa_rd.R
+++ R/verification_multiple/msa_rd.R
@@ -1,15 +1,5 @@
 source("verification_multiple/for_acc/VerifForEach.R")
 
-if (0) {
-  for (i in 1:100) {
-    dir <- paste("rd-etr_", i, sep = "")
-    dir.create(dir)
-    for (p in -3:-5) {
-      verif(method = "rd", output = paste(dir, "/", "rd_p", p, ".rlt", sep = ""), p = p)
-    }
-  }
-}
-
 for (p in -3:-5) {
   dir <- paste("../../Alignment/rd-p_", p, sep = "")
   dir.create(dir)
