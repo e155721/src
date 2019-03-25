1e76f68cdd9d07f73d9028bb0187271d57bebdb7
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-03-25 12:21:01 +0900

    [fix] fixed the process to make the output path
    modified:   verification_multiple/msa_rd.R

--- R/verification_multiple/msa_rd.R
+++ R/verification_multiple/msa_rd.R
@@ -11,7 +11,7 @@ if (0) {
 }
 
 for (p in -3:-5) {
-  dir <- paste("../../Alignment/rd-p_", i, sep = "")
+  dir <- paste("../../Alignment/rd-p_", p, sep = "")
   dir.create(dir)
   for (i in 1:100) {
     verif(method = "rd", output = paste(dir, "/", "rd-etr_", i, ".rlt", sep = ""), p = p)
