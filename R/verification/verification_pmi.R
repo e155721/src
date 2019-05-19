cdcfbf2593f1dccb9e45490de2b3b76d6afec3bc
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-05-20 02:00:15 +0900

    [modify] The output path of fiverification_pmi.R was fixed.
    modified:   R/verification/verification_pmi.R

--- R/verification/verification_pmi.R
+++ R/verification/verification_pmi.R
@@ -14,7 +14,7 @@ filesPath <- GetPathList()
 ansrate.file <- "../../Alignment/ansrate_levenshtein.txt"
 
 # result path
-output.dir <- paste("../../Alignment/pairwise_", format(Sys.Date()), "/", sep = "")
+output.dir <- paste("../../Alignment/pairwise_pmi", format(Sys.Date()), "/", sep = "")
 if (!dir.exists(output.dir)) {
   dir.create(output.dir)
 }
