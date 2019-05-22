fa9c2f80de7b28003d4716b9dc803f1b77db9032
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-05-22 19:52:55 +0900

    [fix] MakeFeatureMatrix() function was fixed because this function was same from MakeEditDistance() function.
    modified:   needleman_wunsch/MakeFeatureMatrix.R

--- R/needleman_wunsch/MakeFeatureMatrix.R
+++ R/needleman_wunsch/MakeFeatureMatrix.R
@@ -28,59 +28,54 @@ MakeFeatureMatrix <- function(s5 = NA, p = NA)
   feature_matrix["-", ] <- p
   feature_matrix[, "-"] <- p
   feature_matrix["-", "-"] <- 0
+    
+  # consonant feature
+  c_feature <- read.table(cValFile)
+  k <- dim(c_feature)[[1]]
+  l <- dim(c_feature)[[2]]
   
-  if (0) {
-    # consonant feature
-    c_feature <- read.table(cValFile)
-    k <- dim(c_feature)[[1]]
-    l <- dim(c_feature)[[2]]
-    
-    c <- matrix(NA, k, l, dimnames = list(consonant, c()))
-    for (i in 1:l) {
-      c[, i] <- c_feature[, i]
-    }
-    
-    c_match <- matrix(NA, k, k)
-    for (i in 1:k) {
-      for (j in 1:k) {
-        match <- 0
-        for (n in 1:5) {
-          if (c[i, n] == c[j, n])
-            match <- match + 1
-        }
-        c_match[i, j] <- match
+  c <- matrix(NA, k, l, dimnames = list(consonant, c()))
+  for (i in 1:l) {
+    c[, i] <- c_feature[, i]
+  }
+
+  c_match <- matrix(NA, k, k)
+  for (i in 1:k) {
+    for (j in 1:k) {
+      match <- 0
+      for (n in 1:5) {
+        if (c[i, n] == c[j, n])
+          match <- match + 1
       }
+      c_match[i, j] <- match
     }
-    
-    # vowel feature
-    v_feature <- read.table(vValFile)
-    k <- dim(v_feature)[[1]]
-    l <- dim(v_feature)[[2]]
-    
-    v <- matrix(NA, k, l)
-    for (i in 1:l) {
-      v[, i] <- v_feature[, i]
-    }
-    
-    v_match <- matrix(NA, k, k)
-    for (i in 1:k) {
-      for (j in 1:k) {
-        match <- 0
-        for (n in 1:5) {
-          if (v[i, n] == v[j, n])
-            match <- match + 1    
-        }
-        v_match[i, j] <- match
+  }
+  
+  # vowel feature
+  v_feature <- read.table(vValFile)
+  k <- dim(v_feature)[[1]]
+  l <- dim(v_feature)[[2]]
+  
+  v <- matrix(NA, k, l)
+  for (i in 1:l) {
+    v[, i] <- v_feature[, i]
+  }
+  
+  v_match <- matrix(NA, k, k)
+  for (i in 1:k) {
+    for (j in 1:k) {
+      match <- 0
+      for (n in 1:5) {
+        if (v[i, n] == v[j, n])
+          match <- match + 1    
       }
+      v_match[i, j] <- match
     }
   }
   
   ###
-  # feature_matrix[1:81, 1:81] <- c_match
-  feature_matrix[1:81, 1:81] <- 1
-  # feature_matrix[82:118, 82:118] <- v_match
-  feature_matrix[82:118, 82:118] <- 1
-  diag(feature_matrix) <- 0
+  feature_matrix[1:81, 1:81] <- c_match
+  feature_matrix[82:118, 82:118] <- v_match
   
   return(feature_matrix)
 }
