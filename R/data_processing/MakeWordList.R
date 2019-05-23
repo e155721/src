6e0fe8c263c91a1d0bff798faf37b752582eb455
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-05-23 21:54:51 +0900

    [refactor] The MakeWordList.R was refactored.
    modified:   data_processing/MakeWordList.R
    new file:   data_processing/MakeWordList.R.old

--- R/data_processing/MakeWordList.R
+++ R/data_processing/MakeWordList.R
@@ -1,33 +1,16 @@
-# make word list
 MakeWordList <- function(file)
 {
-  # load words data
-  sheet <- read.table(file, sep = " ")
-  sheet <- as.matrix(sheet)
-  
-  # get rows number of data sheet
-  nrow <- dim(sheet)[1]
-  
-  # get region name symbols
-  regVec <- as.vector(sheet[, 1])
-  regVec <- regVec[!is.na(regVec)]
-  sheet <- sheet[, -1]
-  
-  word_list <- list()
-  k <- 1
-  for (i in 1:nrow) {
-    tmp_vector <- as.vector(sheet[i, ])
-    tmp_vector <- tmp_vector[!is.na(tmp_vector)]
-    if (length(tmp_vector) != 0) {
-      word_list[[k]] <- t(as.matrix(tmp_vector))
-      k <- k + 1
+  x <- read.table(file)
+  N <- dim(x)[1]
+  word.list <- list()
+  for (i in 1:N) {
+    word.list[[i]] <- as.matrix(x[i, !is.na(x[i, ])])
+    miss.exist <- sum(word.list[[i]]!="-9")
+    if (miss.exist == 0) {
+      word.list[[i]] <- NA
     }
   }
+  word.list <- word.list[!is.na(word.list)]  
   
-  len <- length(regVec)
-  for (i in 1:len) {
-    word_list[[i]] <- cbind(regVec[i], word_list[[i]])
-  }
-  
-  return(word_list)
+  return(word.list)
 }
