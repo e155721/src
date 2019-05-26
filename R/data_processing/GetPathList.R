0b892b4fb0bb5b541d7e8297bebeba13772c45ce
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-05-26 18:01:07 +0900

    [refactor] The GetPathList.R was refactored.
    modified:   data_processing/GetPathList.R

--- R/data_processing/GetPathList.R
+++ R/data_processing/GetPathList.R
@@ -1,24 +1,16 @@
-GetPathList <- function(inputDir = "../../Alignment/input_data/",
-                         correctDir = "../../Alignment/correct_data/")
+GetPathList <- function(inputDir = "../../Alignment/org_data/")
 {
   # input files
   inputFiles <- list.files(inputDir)
   inputPaths <- paste(inputDir, inputFiles, sep = "/")
   
-  # correct files
-  correctFiles <- list.files(correctDir)
-  correctPaths <- paste(correctDir, correctFiles, sep = "/")
-  
   # make the list of file paths
   pathList <- list()
   numOfFiles <- length(inputFiles)
   for (i in 1:numOfFiles) {
-    pathList[[i]] <- c(NA, NA, NA)
-    names(pathList[[i]]) <- c("input", "correct", "name")
-
+    pathList[[i]] <- c(NA)
+    names(pathList[[i]]) <- c("input")
     pathList[[i]]["input"] <- inputPaths[[i]]
-    pathList[[i]]["correct"] <- correctPaths[[i]]
-    pathList[[i]]["name"] <- inputFiles[[i]]
   }
   
   return(pathList)
