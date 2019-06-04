03734a7738f7ab4c4c2b7c839f1f066ab6071d7d
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-06-04 18:41:43 +0900

    [modify] The script of prepare_data.sh was modified for ExtractExcelData.R
    modified:   prepare_data.sh

--- shell/prepare_data.sh
+++ shell/prepare_data.sh
@@ -11,8 +11,8 @@ do
     [ $l[1] -le 2 ] && rm $f
 done
 
-for f in *.org
-do
-    <"$f" | sed -e 's/"-"/NA/g' | sed -e 's/"-9"/NA/g' >"$f:r".input
-    <"$f" | sed -e 's/"-9"/NA/g' >"$f:r".correct
-done
+# for f in *.org
+# do
+#    <"$f" | sed -e 's/"-"/NA/g' | sed -e 's/"-9"/NA/g' >"$f:r".input
+#    <"$f" | sed -e 's/"-9"/NA/g' >"$f:r".correct
+# done
