7aa8900d508d3788a82601b866eff2d990e62e1a
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-06-04 18:44:23 +0900

    [update] The script to extract excel data was updated.
    modified:   data_processing/data_preparing/ExtractExcelData.R

--- R/data_processing/data_preparing/ExtractExcelData.R
+++ R/data_processing/data_preparing/ExtractExcelData.R
@@ -2,11 +2,19 @@
 library(openxlsx)
 library(data.table)
 
+library(foreach)
+library(doParallel)
+registerDoParallel(detectCores())
+
 # FormatData
 FormatData <- function(sheet)
 {
   # delete the labels and the assumed form
   sheet <- sheet[-1:-2, ]
+  # redundant region
+  sheet <- sheet[, -1:-2]
+  # redundant some information
+  sheet <- sheet[, -2:-9]
   
   dim <- dim(sheet)
   x <- matrix(NA, dim[1], dim[2])
@@ -30,7 +38,7 @@ FormatData <- function(sheet)
 
 # ExcelDataExtraction
 ExtractExcelData <- function(input_path = "../../Data/fix_test_data.xlsm",
-                                output_path = "../../Alignment/org_data/")
+                             output_path = "../../Alignment/org_data/")
 {
   if (!dir.exists(output_path)) {
     dir.create(output_path)
@@ -40,21 +48,21 @@ ExtractExcelData <- function(input_path = "../../Data/fix_test_data.xlsm",
   sheet_names <- getSheetNames(input_path)
   sheet_names_length <- length(sheet_names)
   for (i in 1:sheet_names_length) {
-    sheet_names[i] <- gsub("*[ ]*", "", sheet_names[i])
+    sheet_names[[i]] <- gsub("*[ ]*", "", sheet_names[[i]])
+    sheet_names[[i]] <- gsub("（", "(", sheet_names[[i]])
+    sheet_names[[i]] <- gsub("）", ")", sheet_names[[i]])
   }
-  
-  # get the name symbols for each region
-  regions <- read.xlsx(input_path, sheet = i)[, 2]
   
   # output sheets
   k <- 1
-  for (i in 6:135) {
-    sheet <- read.xlsx(input_path, sheet = i)[, 12:27]
-    sheet <- transform(sheet, Regions = regions)
-    setcolorder(sheet, "Regions")
+  foreach (i = 6:135) %dopar% {
+    sheet <- read.xlsx(input_path, sheet = i)[, 1:27]
+    
+    #sheet <- transform(sheet, Regions = regions)
+    #setcolorder(sheet, "Regions")
     sheet <- FormatData(sheet)
-    # write.table(sheet, paste(output_path, sheet_names[i], ".org", sep = ""))
-    write.table(sheet, paste(output_path, formatC(k, width = 3, flag = 0), ".org", sep = ""))
+    write.table(sheet, paste(output_path, sheet_names[i], ".org", sep = ""))
+    # write.table(sheet, paste(output_path, formatC(k, width = 3, flag = 0), ".org", sep = ""))
     k <- k + 1
   }
   
