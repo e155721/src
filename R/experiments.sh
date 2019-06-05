76857ade20a303deb66bc71c1c88862fcebb59f8
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-06-05 15:40:36 +0900

    [add] The experiments.sh was added.

---
+++ R/experiments.sh
@@ -0,0 +1,6 @@
+1;95;0c#!/bin/sh
+
+Rscript verification/verification_pairwise.R
+Rscript verification/verification_levenshtein.R
+Rscript verification/verification_swap.R
+Rscript verification/verification_pmi.R
