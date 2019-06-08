aeed6225e51ceba1561023c372e9d52c3d067b7e
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-06-08 10:34:55 +0900

    [modify] The OutputAlignmentCheck.R will output the line number.
    modified:   verification/verif_lib/OutputAlignmentCheck.R

--- R/verification/verif_lib/OutputAlignmentCheck.R
+++ R/verification/verif_lib/OutputAlignmentCheck.R
@@ -5,19 +5,19 @@ OutputAlignmentCheck <- function(f, output.path, ext, x, y)
   match <- 0  
   i <- 1
   while (i <= N) {
-    x.aln <- paste(x[[i]][1, -1], x[[i+1]][1, -1], collapse = "")
-    y.aln <- paste(y[[i]][1, -1], y[[i+1]][1, -1], collapse = "")
-    region <- paste(x[[i]][1, 1], x[[i+1]][1, 1], sep = "/" )
+    x.aln <- paste(x[[i]], x[[i+1]], collapse = "")
+    y.aln <- paste(y[[i]], y[[i+1]], collapse = "")
+    region <- paste(x[[i]], x[[i+1]][1, 1], sep = "/" )
     
     if (x.aln == y.aln) {
       sink(paste(output.path, gsub("\\..*$", "", f), ext, sep = ""), append = T)
-      print(paste(region, "Match"))
-      cat ("\n\n")
+      print(paste("Match:", i, region))
+      cat ("\n")
       sink()
     } else {
       sink(paste(output.path, gsub("\\..*$", "", f), ext, sep = ""), append = T)
-      print(paste(region, "Mis"))
-      cat ("\n\n")
+      print(paste("Mis", i, region))
+      cat ("\n")
       sink()
     }
     i <- i+2
