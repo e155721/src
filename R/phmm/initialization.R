877a452e203009edaaf08c14ac59b5c9d8e01eaa
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-09-14 22:22:17 +0900

    [add] Added the function for assigning to the transition matrix A.

--- R/phmm/initialization.R
+++ R/phmm/initialization.R
@@ -4,6 +4,37 @@ library(gtools)
 source("lib/load_data_processing.R")
 source("lib/load_verif_lib.R")
 
+Asign2A <- function(params, S) {
+  # Assignes the parameters to the matrix.
+  #
+  # Args:
+  #   params: The transition parameters.
+  #   S: The set of emission states.
+  #
+  # Returns:
+  #   The transition matrix.
+  delta <- params["delta"]
+  epsilon <- params["epsilon"]
+  lambda <- params["lambda"]
+  tau.M <- params["tau.M"]
+  tau.XY <- params["tau.XY"]
+  m.m <- 1-2*delta-tau.M
+  xy.m <- 1-epsilon-lambda-tau.XY
+  
+  N <- length(S)
+  A <- matrix(0, N, N, dimnames = list(S, S))
+  A["M", "M"] <- m.m
+  A["M", "X"] <- A["M", "Y"] <- delta
+  A["M", "End"] <- tau.M
+  
+  A["X", "X"] <- A["Y", "Y"] <- epsilon
+  A["X", "Y"] <- A["Y", "X"] <- lambda
+  A["X", "End"] <- A["Y", "End"] <- tau.XY
+  A["X", "M"] <- A["Y", "M"] <- xy.m
+  
+  return(A)  
+}
+
 wl <- MakeWordList("../../Alignment/org_data/01-003首(2-2).org")
 wl <- MakeGoldStandard(wl)
 wl <- MakeInputSeq(wl)
@@ -41,17 +72,7 @@ names(q.y) <- Sig
 params <- as.vector(rdirichlet(1, matrix(1,1,5)))
 names(params) <- c("delta", "epsilon", "lambda", "tau.XY", "tau.M")
 
-A <- matrix(NA, N, N, dimnames = list(S, S))
-A["M", "M"] <- 1-2*params["delta"]-params["tau.M"]
-A["M", "X"] <- A["M", "Y"] <- params["delta"]
-A["M", "End"] <- params["tau.M"]
-
-A["X", "X"] <- A["Y", "Y"] <- params["epsilon"]
-A["X", "Y"] <- A["Y", "X"] <- params["lambda"]
-A["X", "End"] <- A["Y", "End"] <- params["tau.XY"]
-A["X", "M"] <- A["Y", "M"] <- 1-params["epsilon"]-params["lambda"]-params["tau.XY"]
-
-A["End", ] <- 0
+A <- Asign2A(params, S)
 
 pi <- c(1-2*params["delta"]-params["tau.M"], params["delta"], params["delta"], 0)  # initial probability
 names(pi) <- S
