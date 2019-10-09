c24c2fb7c9d239f4e3eb74583d1dd5f19ef0c385
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-10-09 10:14:49 +0900

    [modify] Refactored MakeCorpus.R.

--- R/verification/methods/MakeCorpus.R
+++ R/verification/methods/MakeCorpus.R
@@ -1,14 +1,22 @@
-MakeCorpus <- function(psa.aln)
-{
+MakeCorpus <- function(psa.aln) {
+  # Makes the corpus to calculate PMI.
+  #
+  # Args:
+  #   psa.aln: A list of an pairwise sequence aligment resutls.
+  #
+  # Returns:
+  #   A corpus to calculate PMI.
   N <- length(psa.aln)
-  i <- 3
-  seq1 <- t(as.matrix(psa.aln[[1]][-1], drop = F))
-  seq2 <- t(as.matrix(psa.aln[[2]][-1], drop = F))
+  seq1 <- NULL
+  seq2 <- NULL
+  
+  i <- 1
   while (i <= N) {
-    seq1 <- cbind(seq1, t(as.matrix(psa.aln[[i]][-1], drop = F)))
-    seq2 <- cbind(seq2, t(as.matrix(psa.aln[[i+1]][-1], drop = F)))
+    seq1 <- cbind(seq1, psa.aln[[i]][1, -1, drop=F])
+    seq2 <- cbind(seq2, psa.aln[[i+1]][1, -1, drop=F])
     i <- i+2  
   }
   corpus <- rbind(seq1, seq2)
+  
   return(corpus)
 }
