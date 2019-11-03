f3b46fc4ceeac59dfe1f21eed584397e853ba159
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-11-03 23:37:46 +0900

    [modify] Refactored test_msa.R

--- R/test/test_msa.R
+++ R/test/test_msa.R
@@ -5,11 +5,10 @@ source("lib/load_scoring_matrix.R")
 LoadLib("msa/")
 
 #word.list <- MakeWordList("../../Alignment/org_data/01-003首(2-2).org")
-word.list <- MakeWordList("../../Alignment/org_csv/01-003首(2-2).csv")
+word.list <- MakeWordList("../../Alignment/org_data/01-003首(2-2).org", csv=F)
 word.list <- MakeInputSeq(word.list)
 
 s <- MakeFeatureMatrix(-Inf, -3)
-s["-", "-"] <- 0
 
 RemoveFirst(word.list ,s)
 BestFirst(word.list ,s)
