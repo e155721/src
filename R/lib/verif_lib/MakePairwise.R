4af6e0b54d5fcb669a315013e337d28be1c158d8
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-11-07 15:12:34 +0900

    [modify] The MakePairwise() is parallelized.

--- R/lib/verif_lib/MakePairwise.R
+++ R/lib/verif_lib/MakePairwise.R
@@ -7,7 +7,7 @@ MakePairwise <- function(word.list, s, select.min=F)
   reg.comb <- combn(1:regions, 2)
   N <- dim(reg.comb)[2]
   
-  psa.list <- foreach (j = 1:N) %do% {
+  psa.list <- foreach (j = 1:N) %dopar% {
     i <- reg.comb[, j]
     psa <- NeedlemanWunsch(as.matrix(word.list[[i[1]]], drop = F),
                            as.matrix(word.list[[i[2]]], drop = F), s, select.min)
