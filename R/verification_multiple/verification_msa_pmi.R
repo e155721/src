9ff8ea441ab53e74a93f5d3de2841afe8ce0e2e1
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-11-14 13:43:09 +0900

    [refactor] Refactored verification_msa_pmi.R

--- R/verification_multiple/verification_msa_pmi.R
+++ R/verification_multiple/verification_msa_pmi.R
@@ -2,6 +2,7 @@ source("lib/load_data_processing.R")
 source("lib/load_verif_lib.R")
 source("lib/load_scoring_matrix.R")
 source("msa/BestFirst.R")
+source("psa/new_pmi.R")
 
 files <- GetPathList()
 
@@ -19,15 +20,20 @@ if (!dir.exists(output.dir)) {
   dir.create(output.dir)
 }
 
+# Compute the scoring matrix using the PMI method.
+input.list <- MakeInputList(files)
 s <- MakeEditDistance(Inf)
+s <- PairwisePMI(input.list, s)
+
 for (file in files) {
   
   gold.list <- MakeWordList(file["input"])  # gold alignment
   input.list <- MakeInputSeq(gold.list)  # input sequences
-  
+
   # Computes the MSA using the BestFirst method.
   print(paste("Start:", file["name"]))
-  msa <- BestFirst(input.list, s, "PMI")
+  psa.init <- ProgressiveAlignment(input.list, s, similarity=F)
+  msa <- BestFirst(psa.init, s, similarity=F)
   print(paste("End:", file["name"]))
   
   # Checks the accuracy of MSA.
