76a089367e0f343779ad8224e27a821c339b2f31
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-11-23 17:02:36 +0900

    [modify] The feature values were converted.

--- R/lib/phoneme/phoneme.R
+++ R/lib/phoneme/phoneme.R
@@ -11,3 +11,10 @@ dimnames(C.feat) <- list(C, NULL)
 # Vowel features
 V.feat <- as.matrix(read.table("lib/phoneme/features/vowels.txt"))
 dimnames(V.feat) <- list(V, NULL)
+
+for (j in 1:5) {
+  C.feat[, j] <- paste("C", C.feat[, j], j, sep="")
+  V.feat[, j] <- paste("V", V.feat[, j], j, sep="")
+}
+CV.feat <- rbind(C.feat, V.feat)
+dimnames(CV.feat) <- list(c(C, V), NULL)
