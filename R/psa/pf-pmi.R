913deb3dae81f03bad3d276877371d286b8405fc
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-11-23 19:42:31 +0900

    [refactor] Rractored the pf-pmi.R.

--- R/psa/pf-pmi.R
+++ R/psa/pf-pmi.R
@@ -1,7 +1,7 @@
 source("lib/load_phoneme.R")
 
 sym2feat <- function(x) {
-  return(CV.feat[x, ])
+  return(mat.CV.feat[x, ])
 }
 
 CalcPFPMI <- function(psa.list, s) {
@@ -22,39 +22,37 @@ CalcPFPMI <- function(psa.list, s) {
   corpus <- corpus[, -which(corpus[2, ] == "-"), drop=F]
   
   ### Convert the symbols to features ###
-  kind.C.feat <- unique(as.vector(C.feat))
-  kind.V.feat <- unique(as.vector(V.feat))
-  kind.CV.feat <- c(kind.C.feat, kind.V.feat)
-  num.kind.CV.feat <- length(kind.CV.feat)
-  feat.mat <- matrix(NA, num.kind.CV.feat, num.kind.CV.feat, dimnames=list(kind.CV.feat, kind.CV.feat))
+  num.CV.feat <- length(CV.feat)
+  feat.mat <- matrix(NA, num.CV.feat, num.CV.feat, dimnames=list(CV.feat, CV.feat))
+  dimnames(mat.CV.feat) <- list(c(C, V), NULL)
   
   corpus.feat <- t(apply(corpus, 1, sym2feat))
   
   # Compute the PMI for each pair.
   feat <- unique(as.vector(corpus.feat))
   feat <- permutations(length(feat), 2, v=feat, repeats.allowed=T)
-  len <- dim(feat)[1]
+  num.feat.pair <- dim(feat)[1]
   score.vec <- list()
-  pmi.list <- foreach(i = 1:len) %dopar% {
+  pmi.list <- foreach(i = 1:num.feat.pair) %dopar% {
     score.vec$V1 <- feat[i, 1]
     score.vec$V2 <- feat[i, 2]
     score.vec$pmi <- PMI(feat[i, 1], feat[i, 2], corpus.feat, E)
     return(score.vec)
   }
   
-  pmi.tmp <- foreach(i = 1:len, .combine = c) %dopar%
+  pmi.tmp <- foreach(i = 1:num.feat.pair, .combine = c) %dopar%
     pmi.list[[i]]$pmi
   pmi.max <- max(pmi.tmp)
   pmi.min <- min(pmi.tmp)
   
-  sym <- unique(as.vector(corpus))
-  for (i in 1:len)
+  for (i in 1:num.feat.pair)
     feat.mat[pmi.list[[i]]$V1, pmi.list[[i]]$V2] <- (pmi.list[[i]]$pmi - pmi.min) / (pmi.max - pmi.min)
   
   # Convert the PMI to the weight of edit operations.
+  sym <- unique(as.vector(corpus))
   for (i in sym)
     for (j in sym)
-      s[i, j] <- sum(diag(feat.mat[CV.feat[i, ], CV.feat[j, ]]))
+      s[i, j] <- sum(diag(feat.mat[mat.CV.feat[i, ], mat.CV.feat[j, ]]))
   
   s[C, V] <- -Inf
   s[V, C] <- -Inf
