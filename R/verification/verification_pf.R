0863cde01a504646e4f6bd40ca42efb969cb4ecb
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-12-15 22:40:08 +0900

    [fix] Fixed the name of the output directory.

--- R/verification/verification_pf.R
+++ R/verification/verification_pf.R
@@ -6,10 +6,10 @@ dir <- "pairwise_pf"
 
 for (pen in -1) {
   # Set the path of the matching rate.
-  ansrate.file <- paste("../../Alignment/", file, "_", format(Sys.Date()), "_", pen, ".txt", sep = "")
+  ansrate.file <- paste("../../Alignment/", file, "_", pen, "_", format(Sys.Date()), ".txt", sep = "")
   
   # Set the path of the PSA directory.
-  output.dir <- paste("../../Alignment/", dir, "_", pen, "_", format(Sys.Date()), "_", pen, "/", sep = "")
+  output.dir <- paste("../../Alignment/", dir, "_", pen, "_", format(Sys.Date()), "/", sep = "")
   if (!dir.exists(output.dir))
     dir.create(output.dir)
   
