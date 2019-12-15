f80b85f024fc48a8616f13378c5ec1b4cb0be7a9
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-12-15 14:33:52 +0900

    [refactor] Refactored the msa_pf-pmi.R.

--- R/verification_multiple/msa_pf-pmi.R
+++ R/verification_multiple/msa_pf-pmi.R
@@ -4,60 +4,69 @@ source("lib/load_scoring_matrix.R")
 source("msa/BestFirst.R")
 source("psa/pmi.R")
 source("psa/pf-pmi.R")
+source("test/check_score.R")
 
 files <- GetPathList()
 
 ansrate <- "ansrate_msa"
 multiple <- "multiple"
 
-for (i in 1:5) {
-  accuracy.mat <- matrix(NA, length(files), 2)
+# matchingrate path
+ansrate.file <- paste("../../Alignment/", ansrate, "_pf-pmi_", format(Sys.Date()), ".txt", sep = "")
+
+# result path
+output.dir <- paste("../../Alignment/", multiple, "_pf-pmi_", format(Sys.Date()), "/", sep = "")
+if (!dir.exists(output.dir))
+  dir.create(output.dir)
+
+# Compute the scoring matrix using the PMI method.
+input.list <- MakeInputList(files)
+load("../../Alignment/scoring_matrices.RData")
+
+accuracy.mat <- matrix(NA, length(files), 2)
+for (file in files) {
   
-  # matchingrate path
-  ansrate.file <- paste("../../Alignment/", ansrate, "_pf-pmi_p", i, "_", format(Sys.Date()), ".txt", sep = "")
+  gold.list <- MakeWordList(file["input"])  # gold alignment
+  input.list <- MakeInputSeq(gold.list)  # input sequences
   
-  # result path
-  output.dir <- paste("../../Alignment/", multiple, "_pf-pmi_p", i, "_", format(Sys.Date()), "/", sep = "")
-  if (!dir.exists(output.dir))
-    dir.create(output.dir)
-  
-  # Compute the scoring matrix using the PMI method.
-  input.list <- MakeInputList(files)
-  load(paste("../../Alignment/12-11_ex-02/scoring_matrix_p", i, ".RData", sep=""))
-  
-  for (file in files) {
-    
-    gold.list <- MakeWordList(file["input"])  # gold alignment
-    input.list <- MakeInputSeq(gold.list)  # input sequences
-    
-    # Computes the MSA using the BestFirst method.
-    print(paste("Start:", file["name"]))
-    psa.init <- ProgressiveAlignment(input.list, s, similarity=F)
-    msa <- BestFirst(psa.init, s, similarity=F)
-    print(paste("End:", file["name"]))
-    
-    # Checks the accuracy of MSA.
-    gold.mat <- DelGap(list2mat(gold.list))
-    gold.mat <- gold.mat[order(gold.mat[, 1]), ]
-    msa <- msa[order(msa[, 1]), ]
-    
-    # Calculates the MSA accuracy.
-    N <- dim(msa)[1]
-    matched <- 0
-    for (i in 1:N) {
-      aligned <- paste(msa[i, ], collapse = " ")
-      gold <- paste(gold.mat[i, ], collapse = " ")
-      if (aligned == gold)
-        matched <- matched + 1
-    }
-    matching.rate <- (matched / N) * 100
-    accuracy.mat <- rbind(accuracy.mat, c(file[["name"]], matching.rate))
-    
-    # Outputs the MSA.  
-    write.table(msa, paste(output.dir, gsub("\\..*$", "", file["name"]), ".aln", sep=""), quote=F)
-    write.table(gold.mat, paste(output.dir, gsub("\\..*$", "", file["name"]), ".lg", sep=""), quote=F)
+  # Computes the MSA using the BestFirst method.
+  msa.list <- list()
+  print(paste("Start:", file["name"]))
+  for (p in 1:5) {
+    psa.init <- ProgressiveAlignment(input.list, s.list[[p]], similarity=F)
+    msa.list[[p]] <- BestFirst(psa.init, s.list[[p]], similarity=F)
   }
+  print(paste("End:", file["name"]))
   
-  # Outputs the accuracy file.
-  write.table(accuracy.mat[-1:-length(files), , drop=F], ansrate.file, quote=F)
+  n <- length(msa.list[[1]])
+  score <- c(0, 0, 0, 0, 0)
+  for (p in 1:5)
+    score[p] <- score[p] + CheckScore(msa.list[[p]], s.list[[p]])
+  
+  score.min <- which(score==min(score))[1]
+  msa <- msa.list[[score.min]]
+  
+  # Checks the accuracy of MSA.
+  gold.mat <- DelGap(list2mat(gold.list))
+  gold.mat <- gold.mat[order(gold.mat[, 1]), ]
+  msa <- msa[order(msa[, 1]), ]
+  
+  # Calculates the MSA accuracy.
+  N <- dim(msa)[1]
+  matched <- 0
+  for (i in 1:N) {
+    aligned <- paste(msa[i, ], collapse = " ")
+    gold <- paste(gold.mat[i, ], collapse = " ")
+    if (aligned == gold)
+      matched <- matched + 1
+  }
+  matching.rate <- (matched / N) * 100
+  accuracy.mat <- rbind(accuracy.mat, c(file[["name"]], matching.rate))
+  
+  # Outputs the MSA.  
+  write.table(msa, paste(output.dir, gsub("\\..*$", "", file["name"]), ".aln", sep=""), quote=F)
+  write.table(gold.mat, paste(output.dir, gsub("\\..*$", "", file["name"]), ".lg", sep=""), quote=F)
 }
+
+# Outputs the accuracy file.
+write.table(accuracy.mat[-1:-length(files), , drop=F], ansrate.file, quote=F)
