993637b6687bc60c70a4c2e80d83be9fbb098227
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-12-15 23:09:57 +0900

    [refactor] Refactored the verification_pmi.R.

--- R/verification/verification_pmi.R
+++ R/verification/verification_pmi.R
@@ -10,17 +10,17 @@ dir <- "pairwise_pmi"
 # Set the path of the matching rate.
 ansrate.file <- paste("../../Alignment/", file, "_", format(Sys.Date()), ".txt", sep = "")
 
+# Set the path of the PSA directory.
+output.dir <- paste("../../Alignment/", dir, "_", format(Sys.Date()), "/", sep = "")
+if (!dir.exists(output.dir))
+  dir.create(output.dir)
+
 # Update the scoring matrix with PMI.
 file.list <- GetPathList()
 input.list <- MakeInputList(file.list)
 s <- MakeEditDistance(Inf)
 s <- PairwisePMI(input.list, s)
 save(s, file=paste("scoring_matrix_pmi_", format(Sys.Date()), ".RData", sep=""))
-
-# Set the path of the PSA directory.
-output.dir <- paste("../../Alignment/", dir, "_", format(Sys.Date()), "/", sep = "")
-if (!dir.exists(output.dir))
-  dir.create(output.dir)
 
 # Execute the PSA for each word.
 VerificationPSA(ansrate.file, output.dir, s)
