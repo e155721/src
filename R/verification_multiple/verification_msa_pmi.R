cb820a8ad4fba70526aa8fbead5d91cddfd972b8
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2019-12-16 09:03:09 +0900

    Refactored the verification_msa_pmi.R.

--- R/verification_multiple/verification_msa_pmi.R
+++ R/verification_multiple/verification_msa_pmi.R
@@ -3,13 +3,10 @@ source("lib/load_verif_lib.R")
 source("lib/load_scoring_matrix.R")
 source("msa/BestFirst.R")
 source("psa/pmi.R")
-
-files <- GetPathList()
+source("verification_multiple/VerificationMSA.R")
 
 ansrate <- "ansrate_msa"
 multiple <- "multiple"
-
-accuracy.mat <- matrix(NA, length(files), 2)
 
 # matchingrate path
 ansrate.file <- paste("../../Alignment/", ansrate, "_pmi_", format(Sys.Date()), ".txt", sep = "")
@@ -21,43 +18,10 @@ if (!dir.exists(output.dir)) {
 }
 
 # Compute the scoring matrix using the PMI method.
+files <- GetPathList()
 input.list <- MakeInputList(files)
 s <- MakeEditDistance(Inf)
 s <- PairwisePMI(input.list, s)
 save(s, file="scoring_matrix_msa_pmi.RData")
 
-for (file in files) {
-  
-  gold.list <- MakeWordList(file["input"])  # gold alignment
-  input.list <- MakeInputSeq(gold.list)  # input sequences
-
-  # Computes the MSA using the BestFirst method.
-  print(paste("Start:", file["name"]))
-  psa.init <- ProgressiveAlignment(input.list, s, similarity=F)
-  msa <- BestFirst(psa.init, s, similarity=F)
-  print(paste("End:", file["name"]))
-  
-  # Checks the accuracy of MSA.
-  gold.mat <- DelGap(list2mat(gold.list))
-  gold.mat <- gold.mat[order(gold.mat[, 1]), ]
-  msa <- msa[order(msa[, 1]), ]
-  
-  # Calculates the MSA accuracy.
-  N <- dim(msa)[1]
-  matched <- 0
-  for (i in 1:N) {
-    aligned <- paste(msa[i, ], collapse = " ")
-    gold <- paste(gold.mat[i, ], collapse = " ")
-    if (aligned == gold)
-      matched <- matched + 1
-  }
-  matching.rate <- (matched / N) * 100
-  accuracy.mat <- rbind(accuracy.mat, c(file[["name"]], matching.rate))
-  
-  # Outputs the MSA.  
-  write.table(msa, paste(output.dir, gsub("\\..*$", "", file["name"]), ".aln", sep=""), quote=F)
-  write.table(gold.mat, paste(output.dir, gsub("\\..*$", "", file["name"]), ".lg", sep=""), quote=F)
-}
-
-# Outputs the accuracy file.
-write.table(accuracy.mat[-1:-length(files), , drop=F], ansrate.file, quote=F)
+VerificationMSA(ansrate.file, output.dir, s, similarity=F)
