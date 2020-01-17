8dc48c4d6dcfd127fe2f2eb0ed8692ed580e572c
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-01-17 18:58:44 +0900

    [refactor] Refactored the MakePairwise.R.

--- R/lib/verif_lib/MakePairwise.R
+++ R/lib/verif_lib/MakePairwise.R
@@ -1,16 +1,24 @@
 source("lib/load_nwunsch.R")
 
-MakePairwise <- function(word.list, s, select.min=F)
-{
-
-  regions <- length(word.list)
-  reg.comb <- combn(1:regions, 2)
-  N <- dim(reg.comb)[2]
+MakePairwise <- function(seq.list, s, select.min=F) {
+  # Make the PSA for all num.regs pair.
+  #
+  # Args:
+  #   seq.list: The list of phonetic strings.
+  #   s: The scoring matrix.
+  #   select.min: Whether the optimal alignment is maximized or minimized.
+  #
+  # Returns:
+  #   The list of PSAs.
+  num.regs <- length(seq.list)
+  combs.regs <- combn(1:num.regs, 2)
+  N <- dim(combs.regs)[2]
   
-  psa.list <- foreach (j = 1:N) %do% {
-    i <- reg.comb[, j]
-    psa <- NeedlemanWunsch(as.matrix(word.list[[i[1]]], drop = F),
-                           as.matrix(word.list[[i[2]]], drop = F), s, select.min)
+  psa.list <- list()
+  for (i in 1:N) {
+    pair.regs <- combs.regs[, i]
+    psa.list[[i]] <- NeedlemanWunsch(as.matrix(seq.list[[pair.regs[1]]], drop = F),
+                                     as.matrix(seq.list[[pair.regs[2]]], drop = F), s, select.min)
   }
   
   return(psa.list)
