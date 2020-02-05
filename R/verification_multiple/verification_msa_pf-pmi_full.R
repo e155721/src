798dfb0dd8c0864aa61d864b710baa2f6720237a
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-02-05 19:40:17 +0900

    [add] Added verification_msa_pf-pmi_full.R.

---
+++ R/verification_multiple/verification_msa_pf-pmi_full.R
@@ -0,0 +1,44 @@
+source("lib/load_data_processing.R")
+source("lib/load_verif_lib.R")
+source("lib/load_scoring_matrix.R")
+source("lib/load_exec_align.R")
+source("msa/BestFirst.R")
+source("psa/pairwise_pmi.R")
+source("psa/pairwise_pf-pmi.R")
+source("verification_multiple/change_list_msa2psa.R")
+source("verification_multiple/VerificationMSA.R")
+
+ansrate <- "ansrate_msa"
+multiple <- "multiple"
+
+# matchingrate path
+ansrate.file <- paste("../../Alignment/", ansrate, "_pmi_", format(Sys.Date()), ".txt", sep = "")
+
+# result path
+output.dir <- paste("../../Alignment/", multiple, "_pmi_", format(Sys.Date()), "/", sep = "")
+if (!dir.exists(output.dir)) {
+  dir.create(output.dir)
+}
+
+# Compute the scoring matrix using the PMI method.
+list.words <- GetPathList()
+s <- MakeEditDistance(Inf)
+psa.list <- PSAforEachWord(list.words, s)
+s <- PairwisePFPMI(psa.list, list.words, s)
+#save(s, file="scoring_matrix_msa_pmi.RData")
+
+s.old <- s
+N <- length(s.old)
+for (i in 1:N) {
+  s.old[i] <- 0
+}
+
+while (1) {
+  diff <- N - sum(s == s.old)
+  if (diff == 0) break
+  msa.list <- MSAforEachWord(list.word, s, similarity=F)
+  psa.list <- ChangeListMSA2PSA(msa.list, s)
+  s.old <- s
+  s <- PairwisePFPMI(psa.list, list.words, s)
+}
+VerificationMSA(ansrate.file, output.dir, s, similarity=F)
