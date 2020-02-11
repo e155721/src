9763ed6d0b9a010f0568ab7fe7791911e98e7084
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-02-12 08:54:11 +0900

    [fix] Fixed verification_msa_pmi_full.R.

--- R/verification_multiple/verification_msa_pmi_full.R
+++ R/verification_multiple/verification_msa_pmi_full.R
@@ -2,6 +2,7 @@ source("lib/load_data_processing.R")
 source("lib/load_verif_lib.R")
 source("lib/load_scoring_matrix.R")
 source("lib/load_exec_align.R")
+source("msa/ProgressiveAlignment.R")
 source("msa/BestFirst.R")
 source("psa/pairwise_pmi.R")
 source("verification_multiple/change_list_msa2psa.R")
@@ -27,18 +28,66 @@ psa.list <- PSAforEachWord(list.words, s)
 s <- PairwisePMI(psa.list, list.words, s)
 #save(s, file="scoring_matrix_msa_pmi.RData")
 
+# For progressive
 s.old <- s
 N <- length(s.old)
 for (i in 1:N) {
   s.old[i] <- 0
 }
 
+pa.list <- list()
 while (1) {
   diff <- N - sum(s == s.old)
   if (diff == 0) break
-  msa.list <- MSAforEachWord(list.word, s, similarity=F)
+  #
+  i <- 1
+  for (w in list.words) {
+    # Make the word list.
+    gold.list <- MakeWordList(w["input"])  # gold alignment
+    seq.list <- MakeInputSeq(gold.list)  # input sequences
+    
+    # Computes the MSA using the BestFirst method.
+    print(paste("Start:", w["name"]))
+    pa.list[[i]] <- list()
+    pa.list[[i]] <- ProgressiveAlignment(seq.list, s, F)
+    i <- i + 1
+    print(paste("End:", w["name"]))
+  } 
+  #
+  psa.list <- ChangeListMSA2PSA(pa.list, s)
+  s.old <- s
+  s <- PairwisePMI(psa.list, list.words, s)
+}
+
+# For best first
+s.old <- s
+N <- length(s.old)
+for (i in 1:N) {
+  s.old[i] <- 0
+}
+
+msa.list <- list()
+while (1) {
+  diff <- N - sum(s == s.old)
+  if (diff == 0) break
+  #
+  i <- 1
+  for (pa in pa.list) {
+    # Make the word list.
+    gold.list <- MakeWordList(w["input"])  # gold alignment
+    seq.list <- MakeInputSeq(gold.list)  # input sequences
+    
+    # Computes the MSA using the BestFirst method.
+    msa.list[[i]] <- list()
+    msa.list[[i]] <- msa.list[[i]] <- BestFirst(pa, s, F)
+    #
+    i <- i + 1
+  } 
+  #
   psa.list <- ChangeListMSA2PSA(msa.list, s)
   s.old <- s
   s <- PairwisePMI(psa.list, list.words, s)
 }
+
+# For verification
 VerificationMSA(ansrate.file, output.dir, s, similarity=F)
