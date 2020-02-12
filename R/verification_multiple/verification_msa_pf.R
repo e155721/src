4ad9571ec16fc232ca4e90df4e23fad7d4585fca
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-02-12 14:16:39 +0900

    [refactor] Refactored verification_msa_pf.R.

--- R/verification_multiple/verification_msa_pf.R
+++ R/verification_multiple/verification_msa_pf.R
@@ -1,24 +1,29 @@
 source("lib/load_data_processing.R")
 source("lib/load_scoring_matrix.R")
-source("msa/ProgressiveAlignment.R")
-source("msa/BestFirst.R")
-source("verification_multiple/VerificationMSA.R")
+source("lib/load_exec_align.R")
+source("verification_multiple/CalcAccMSA.R")
 source("parallel_config.R")
 
 ansrate <- "ansrate_msa_pf"
 multiple <- "multiple_pf"
 
+list.words <- GetPathList()  # the list of words
+
 for (pen in (-1)) {
   
   # matchingrate path
-  ansrate.file <- paste("../../Alignment/", ansrate, "_", format(Sys.Date()), "_", pen, ".txt", sep = "")
+  ansrate.file <- paste("../../Alignment/", ansrate, "_", pen, "_", format(Sys.Date()), ".txt", sep = "")
   
   # result path
-  output.dir <- paste("../../Alignment/", multiple, "_", format(Sys.Date()), "_", pen, "/", sep = "")
+  output.dir <- paste("../../Alignment/", multiple, "_", pen, "_", format(Sys.Date()), "/", sep = "")
   if (!dir.exists(output.dir)) {
     dir.create(output.dir)
   }
-  
+
+  # Make the list of the MSAs.
   s <- MakeFeatureMatrix(-Inf, pen)
-  VerificationMSA(ansrate.file, output.dir, s, similarity=T)
+  msa.list <- MSAforEachWord(list.words, s)
+  # Calculate the accuracy of the MSAs.
+  CalcAccMSA(msa.list, list.words, ansrate.file, output.dir)
+  
 }
