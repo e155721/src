8442e31850f02064f32086704964efe08360f861
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-03-16 07:53:04 +0900

    [fix] Fixed verification_pmi.R.

--- R/verification/verification_pmi.R
+++ R/verification/verification_pmi.R
@@ -1,6 +1,6 @@
 source("lib/load_data_processing.R")
 source("lib/load_scoring_matrix.R")
-source("lib/load_exec_align.R")
+source("psa/psa_for_all_words.R")
 source("lib/load_verif_lib.R")
 source("psa/pairwise_pmi.R")
 source("verification/verification_psa.R")
@@ -14,7 +14,7 @@ path <- MakePath(file, dir, ext)
 # Update the scoring matrix with PMI.
 list.words <- GetPathList()
 s <- MakeEditDistance(Inf)
-psa.list <- PSAforEachWord(list.words, s)
+psa.list <- PSAforAllWrods(s, dist = T)
 psa.list <- PairwisePMI(psa.list, list.words, s)$psa.list
 #save(s, file=paste("scoring_matrix_pmi_", format(Sys.Date()), ".RData", sep=""))
 
