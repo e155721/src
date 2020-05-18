323814ceabb8ad8ae8a717a9292a470681565186
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-05-18 09:40:29 +0900

    [modify] The 'verification_pmi.R' and the 'verification_pf-pmi.R' can save the matrix of the PMIs and the updated scoring matrix.

--- R/verification/verification_pf-pmi.R
+++ R/verification/verification_pf-pmi.R
@@ -9,15 +9,28 @@ source("parallel_config.R")
 
 file <- "ansrate_pf-pmi"
 dir <- "pairwise_pf-pmi"
-ext = commandArgs(trailingOnly=TRUE)[1]
+ext = commandArgs(trailingOnly = TRUE)[1]
 path <- MakePath(file, dir, ext)
 
-# Update the scoring matrix with PMI.
+# Create an itnitial scoring matrix and a list of PSAs.
 list.words <- GetPathList()
-s <- MakeEditDistance(Inf)
-psa.list <- PSAforEachWord(list.words, s, dist = T)
-psa.list <- PairwisePMI(psa.list, list.words, s, UpdatePFPMI)$psa.list
-#save(s, file=paste("scoring_matrix_pmi_", format(Sys.Date()), ".RData", sep=""))
+s          <- MakeEditDistance(Inf)
+psa.list   <- PSAforEachWord(list.words, s, dist = T)
+
+# Update the scoring matrix using the PF-PMI.
+pmi.o    <- PairwisePMI(psa.list, list.words, s, UpdatePFPMI)
+pmi.mat  <- pmi.o$pmi.mat
+s        <- pmi.o$s
+psa.list <- pmi.o$psa.list
 
 # Execute the PSA for each word.
 VerificationPSA(psa.list, path$ansrate.file, path$output.dir)
+
+# Save the matrix of the PMIs and the scoring matrix.
+if (is.na(ext)) {
+  ext <- NULL 
+} else {
+  ext <- paste("_", ext, sep = "")
+}
+save(pmi.mat, file = paste("matrix_psa_PF-PMI", ext, "_", format(Sys.Date()), ".RData", sep = ""))
+save(s, file = paste("score_psa_PF-PMI", ext, "_", format(Sys.Date()), ".RData", sep = ""))
