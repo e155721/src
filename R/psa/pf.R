6f84531989ad881e25a8d374ac8bbd8be756629d
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-09-19 16:05:26 +0900

    [delete] Deleted 'pf.R'.

--- R/psa/pf.R
+++
@@ -1,7 +0,0 @@
-source("lib/load_verif_lib.R")
-
-PairwisePF <- function(word.list, s) {
-  # making the pairwise alignment in all regions
-  psa.aln <- MakePairwise(word.list, s)
-  return(psa.aln)
-}
