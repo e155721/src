d2e10449da3ff4a699df7d05d54d7131110c37c8
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-09-19 16:04:56 +0900

    [delete] Deleted 'levenshtein.R'.

--- R/psa/levenshtein.R
+++
@@ -1,7 +0,0 @@
-source("lib/load_verif_lib.R")
-
-PairwiseLV  <- function(word.list, s) {
-  # making the pairwise alignment in all regions
-  psa.aln <- MakePairwise(word.list, s, select_min = T)
-  return(psa.aln)
-}
