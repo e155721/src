c7c023b9caf3e9e3d64cc398314bed28deea1ea5
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-09-20 01:15:32 +0900

    [modify] 'LoadLib' function reads each package only one time.

--- R/lib/LoadLib.R
+++ R/lib/LoadLib.R
@@ -1,11 +1,24 @@
 LoadLib <- function(path) {
-  lib.path <- paste(getwd(), "/", path, "/", sep = "")
-  files <- list.files(lib.path, pattern = "*.R$")
-  
-  for (f in files) {
-    print(f)
-    source(paste(lib.path, f, sep = ""))
+
+  if (!exists("name_list")) {
+    assign(x = "name_list", value = list(), envir = .GlobalEnv)
   }
-  
+
+  hit <- sum(path == name_list)
+  if (!hit) {
+
+    i <- length(name_list)
+    name_list[[(i + 1)]] <<- path
+
+    lib.path <- paste(getwd(), "/", path, "/", sep = "")
+    files <- list.files(lib.path, pattern = "*.R$")
+
+    for (f in files) {
+      print(f)
+      source(paste(lib.path, f, sep = ""))
+    }
+
+  }
+
   return(0)
 }
