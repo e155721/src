e60d3ebba2aff62d7f6349f0e2e994fe6c445d16
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-04 22:44:10 +0900

    [add] Added 'test.sh'.

---
+++ R/test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# 実行方法：Rscript "Rスクリプト名" "入力ディレクトリ" "出力ディレクトリ"
+
+# PSA
+echo "PSA LV"
+Rscript execution_psa/execution_lv.R "data/" "psa_lv/" >/dev/null
+echo
+
+echo "PSA PF"
+Rscript execution_psa/execution_pf.R "data/" "psa_pf/" >/dev/null
+echo
+
+echo "PSA PMI"
+Rscript execution_psa/execution_pmi.R "data/" "psa_pmi/" >/dev/null
+echo
+
+echo "PSA PF-PMI"
+Rscript execution_psa/execution_pf-pmi.R "data/" "psa_pf-pmi/" >/dev/null
+echo
+
+
+# MSA
+echo "MSA LV"
+Rscript execution_msa/execution_msa_lv.R "data/" "msa_lv/" >/dev/null
+echo
+
+echo "MSA PF"
+Rscript execution_msa/execution_msa_pf.R "data/" "msa_pf/" >/dev/null
+echo
+
+echo "MSA PMI"
+Rscript execution_msa/execution_msa_pmi.R "data/" "msa_pmi/" >/dev/null
+echo
+
+echo "MSA PF-PMI"
+Rscript execution_msa/execution_msa_pf-pmi.R "data/" "msa_pf-pmi/" >/dev/null
