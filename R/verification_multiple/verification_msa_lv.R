47f9e50b3e3236f7b7a4c23d0c3f4f2117338474
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-21 00:27:01 +0900

    [modify] Modified the 'verification_msa_lv.R'.
    This script outputs the regional distance matrix.

--- R/verification_multiple/verification_msa_lv.R
+++ R/verification_multiple/verification_msa_lv.R
@@ -4,6 +4,8 @@ source("lib/load_verif_lib.R")
 source("verification_multiple/verification_msa.R")
 source("parallel_config.R")
 
+source("dist_psa.R")
+
 
 ansrate <- "ansrate_msa_lv"
 multiple <- "multiple_lv"
@@ -17,3 +19,5 @@ word_list <- make_word_list(file_list)
 msa_list <- msa_lv(word_list)
 
 verification_msa(msa_list, file_list, path$ansrate.file, path$output.dir)
+
+make_region_dist(file_list = file_list, output_dir = path$output.dir, method = "ld", s = MakeEditDistance(Inf))
