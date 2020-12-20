6b1dc84bdc9a546667f9b9298b70bf69993ae944
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-20 20:22:10 +0900

    [modify] The 'OutputAlignment' function outputs a PSA with a CSV file.

--- R/lib/data_processing/OutputAlignment.R
+++ R/lib/data_processing/OutputAlignment.R
@@ -1,17 +1,29 @@
-OutputAlignment <- function(f, output.path, ext, psa)
-{
+OutputAlignment <- function(f, output.path, ext, psa) {
   # output alignments
-  N <- length(psa)
-  for (i in 1:N) {
-    # by The Needleman-Wunsch
-    sink(paste(output.path, gsub("\\..*$", "", f), ext, sep = ""), append = T)
-    cat(paste(i, " "))
-    print(paste(psa[[i]]$seq1, collapse = " "), quote = F)
-    cat(paste(i+1, " "))
-    print(paste(psa[[i]]$seq2, collapse = " "), quote = F)
-    cat("\n")
-    sink()
+
+  len_list <- lapply(psa, (function(x){
+    return(dim(x$aln)[2])
+  }))
+
+  M_tmp <- length(psa)
+  M <- (M_tmp * 2) + (M_tmp - 1)
+  N <- max(unlist(len_list))
+
+  psa_mat <- matrix("", M, N)
+  idx <- seq(1, M, 3)
+
+  j <- 1
+  for (i in idx) {
+    psa[[j]]$aln[1:2, 1] <- paste(j, ": ", psa[[j]]$aln[1:2, 1], sep = "")
+    psa_len <- dim(psa[[j]]$aln)[2]
+    psa_mat[i:(i + 1), 1:psa_len] <- psa[[j]]$aln
+    j <- j + 1
   }
-  
+
+  write.csv(psa_mat, file = paste(output.path, gsub("\\..*$", "", f), ext, sep = ""),
+            append = F, quote = T, sep = " ",
+            eol = "\n", na = "NA", dec = ".", row.names = F,
+            col.names = T, qmethod = "double", fileEncoding = "UTF-8")
+
   return(0)
 }
