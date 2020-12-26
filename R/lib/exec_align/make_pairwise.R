318899fe9de9033a1180f8dc9c29e13ca192ca23
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-26 14:31:05 +0900

    [refactor] Refactored the 'MakePairwise' function.

--- R/lib/exec_align/make_pairwise.R
+++ R/lib/exec_align/make_pairwise.R
@@ -1,27 +1,26 @@
 source("lib/load_nwunsch.R")
 
-MakePairwise <- function(seq.list, s, select_min=F) {
-  # Make the PSA for all num.regs pair.
+MakePairwise <- function(seq_list, s, select_min=F) {
+  # Make the PSA for all num_regs pair.
   #
   # Args:
-  #   seq.list: The list of phonetic strings.
-  #   s: The scoring matrix.
+  #   seq_list: A list of phonetic strings.
+  #   s: A scoring matrix.
   #   select_min: Whether the optimal alignment is maximized or minimized.
   #
   # Returns:
   #   The list of PSAs.
-  num.regs <- length(seq.list)
-  combs.regs <- combn(1:num.regs, 2)
-  N <- dim(combs.regs)[2]
+  regions <- combn(1:length(seq_list), 2)
+  N <- dim(regions)[2]
 
   psa_list <- list()
   for (i in 1:N) {
-    pair.regs <- combs.regs[, i]
-    psa_list[[i]] <- needleman_wunsch(as.matrix(seq.list[[pair.regs[1]]], drop = F),
-                                      as.matrix(seq.list[[pair.regs[2]]], drop = F), s, select_min)
+    reg_pair <- regions[, i]
+    psa_list[[i]] <- needleman_wunsch(as.matrix(seq_list[[reg_pair[1]]], drop = F),
+                                      as.matrix(seq_list[[reg_pair[2]]], drop = F), s, select_min)
   }
 
-  attributes(psa_list) <- list(word = attributes(seq.list)$word)
+  attributes(psa_list) <- list(word = attributes(seq_list)$word)
 
   return(psa_list)
 }
