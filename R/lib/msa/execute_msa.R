6f7c0b08c4b1de607da25833ed939f3f23f07b71
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-27 03:51:17 +0900

    [fix] Fixed the 'execute_msa' function.

--- R/lib/msa/execute_msa.R
+++ R/lib/msa/execute_msa.R
@@ -13,28 +13,6 @@ msa_ld <- function(word_list) {
 }
 
 
-
-psa_pmi <- function(fun, word_list, output_dir, cv_sep) {
-
-  # Create an itnitial scoring matrix and a list of PSAs.
-  s         <- MakeEditDistance(Inf)
-  psa_list  <- PSAforEachWord(word_list, s, dist = T)
-
-  # Update the scoring matrix using the PMI.
-  pmi_rlt  <- PairwisePMI(psa_list, word_list, s, fun, cv_sep)
-  pmi_list <- pmi_rlt$pmi_list
-  s        <- pmi_rlt$s
-  psa_list <- pmi_rlt$psa_list
-
-  # Save the matrix of the PMIs and the scoring matrix.
-  method <- attributes(fun)$method
-  save(pmi_list, file = paste(output_dir, "/", "list_psa_", method, ".RData", sep = ""))
-  save(s, file = paste(output_dir, "/", "score_psa_", method, ".RData", sep = ""))
-
-  return(psa_list)
-}
-
-
 msa_pmi <- function(fun, word_list, output_dir, cv_sep) {
 
   # Update the scoring matrix using the PMI.
@@ -67,7 +45,7 @@ execute_msa <- function(method, word_list, output_dir, cv_sep=T) {
   msa_list <- switch(num,
                      "1" = msa_ld(word_list),
                      "2" = msa_pmi(UpdatePMI, word_list, output_dir, cv_sep),
-                     "3" = psa_pmi(UpdatePFPMI, word_list, output_dir, cv_sep)
+                     "3" = msa_pmi(UpdatePFPMI, word_list, output_dir, cv_sep)
   )
 
   return(msa_list)
