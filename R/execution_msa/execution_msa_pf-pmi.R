2de53c927e7287a3eb517e44e437a4975c00b24a
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-26 19:17:44 +0900

    [refactor] Refactored the 'execution_msa_pf-pmi.R'.

--- R/execution_msa/execution_msa_pf-pmi.R
+++ R/execution_msa/execution_msa_pf-pmi.R
@@ -14,8 +14,8 @@ pf_pmi_rlt   <- msa_pf_pmi(word_list, cv_sep = T)
 msa_list     <- pf_pmi_rlt$msa_list
 score_pf_pmi <- pf_pmi_rlt$s
 
-# Output the PSA.
-OutputMSA(msa_list, word_list, output_dir)
+# Output the MSAs.
+output_msa(msa_list, output_dir, ".csv")
 
 # Output the updated scoring matrix.
 save(score_pf_pmi, file = paste(output_dir, "msa_pf-pmi_score_mat.RData", sep = ""))
