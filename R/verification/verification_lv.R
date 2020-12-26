5dbbd4a351497dc2686e2078b7c78b75d0f78865
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-27 01:39:01 +0900

    [refactor] Refactored the 'verification_lv.R'.

--- R/verification/verification_lv.R
+++ R/verification/verification_lv.R
@@ -4,10 +4,14 @@ source("lib/load_verif_lib.R")
 source("parallel_config.R")
 
 
-file <- "ansrate_lv"
-dir <- "pairwise_lv"
-ext <- commandArgs(trailingOnly = TRUE)[1]
-path <- MakePath(file, dir, ext)
+method     <- commandArgs(trailingOnly = TRUE)[1]
+output_dir <- commandArgs(trailingOnly = TRUE)[2]
+
+output_dir <- paste(output_dir, "/", "psa_", method, "/", sep = "")
+if (!dir.exists(output_dir))
+  dir.create(output_dir)
+
+acc_file <- paste(output_dir, "/", "acc_psa_", method, ".txt", sep = "")
 
 word_list      <- make_word_list("../../Alignment/org_data/input.csv")
 word_list_gold <- make_word_list("../../Alignment/org_data/gold.csv")
@@ -16,8 +20,8 @@ psa_list      <- psa_lv(word_list)
 psa_list_gold <- lapply(word_list_gold, make_gold_psa)
 
 # Calculate the PSAs accuracy.
-VerificationPSA(psa_list, psa_list_gold, path$ansrate.file, path$output.dir)
+VerificationPSA(psa_list, psa_list_gold, acc_file, output_dir)
 
 # Output the PSAs.
-output_psa(psa_list, output_dir = path$output.dir, ext = ".csv")
-output_psa(psa_list_gold, output_dir = path$output.dir, ext = "_lg.csv")
+output_psa(psa_list, output_dir = output_dir, ext = ".csv")
+output_psa(psa_list_gold, output_dir = output_dir, ext = "_lg.csv")
