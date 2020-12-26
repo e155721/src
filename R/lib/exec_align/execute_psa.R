74e2112169e2ea35a43a9a106af0ff6f9918212b
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-27 02:22:03 +0900

    [refactor] Refactored the 'execute_psa.R'.

--- R/lib/exec_align/execute_psa.R
+++ R/lib/exec_align/execute_psa.R
@@ -1,3 +1,4 @@
+source("lib/load_pmi.R")
 source("lib/load_scoring_matrix.R")
 source("lib/load_exec_align.R")
 source("parallel_config.R")
@@ -12,13 +13,30 @@ psa_ld <- function(word_list) {
 }
 
 
+psa_pmi <- function(method, word_list, cv_sep) {
+
+  # Create an itnitial scoring matrix and a list of PSAs.
+  s         <- MakeEditDistance(Inf)
+  psa_list  <- PSAforEachWord(word_list, s, dist = T)
+
+  # Update the scoring matrix using the PMI.
+  pmi_rlt  <- PairwisePMI(psa_list, word_list, s, UpdatePMI, cv_sep)
+
+  return(pmi_rlt)
+}
+
+
 execute_psa <- function(method, word_list, output_dir, cv_sep=T) {
 
-  if (method == "ld") {
-    psa_list <- psa_ld(word_list)
-  } else {
+  num <- switch(method,
+                "ld"     = 1,
+                "pmi"    = 2,
+                "pf-pmi" = 3
+  )
 
-  }
+  psa_list <- switch(num,
+                     "1" = psa_ld(word_list)
+  )
 
   return(psa_list)
 }
