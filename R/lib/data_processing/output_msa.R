bf49e4aaabe92277ad34f589fe1ecfc658260f2d
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-26 19:06:13 +0900

    [refactor] Refactored the 'output_msa' function.

--- R/lib/data_processing/output_msa.R
+++ R/lib/data_processing/output_msa.R
@@ -1,31 +1,23 @@
 source("lib/load_data_processing.R")
 
-OutputMSA <- function(msa_list, word_list, output_dir) {
+output_msa <- function(msa_list, output_dir, ext) {
   # Compute the MSA for each word.
   #
   # Args:
   #   msa_list: The list of MSAs.
-  #   word_list: The list of words that were used for the MSA.
   #   output_dir: The path of the MSA directory.
   #
   # Returns:
   #   Nothing.
 
-  word_vec <- NULL
-  M <- length(word_list)
+
+  M <- length(msa_list)
+
   for (i in 1:M) {
-  word_vec[i] <- attributes(word_list[[i]])$word
-  }
 
-  if (dir.exists(paths = output_dir)) {
-    # Do not create the directory.
-  } else {
-    dir.create(output_dir)
-  }
+    word <- attributes(msa_list[[i]])$word
 
-  i <- 1
-  for (w in word_vec) {
-
+    #The MSA will be sorted by the regions.
     msa <- msa_list[[i]]$aln
     msa <- msa[order(msa[, 1]), ]
 
@@ -33,8 +25,9 @@ OutputMSA <- function(msa_list, word_list, output_dir) {
     msa <- Convert(msa)
 
     # Outputs the MSA.
-    write.table(msa, paste(output_dir, w, ".aln", sep = ""), quote = F)
-    i <- i + 1
+    write.csv(msa, file = paste(output_dir, gsub("\\..*$", "", word), ext, sep = ""),
+              quote = T, eol = "\n", na = "NA", row.names = F,
+              fileEncoding = "UTF-8")
   }
 
   return(0)
