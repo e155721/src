46a67581444c45fb9586994c3e3248caa8ab9f53
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-27 02:39:02 +0900

    [refactor] Refactored the 'execute_psa.R'.

--- R/lib/exec_align/execute_psa.R
+++ R/lib/exec_align/execute_psa.R
@@ -13,14 +13,14 @@ psa_ld <- function(word_list) {
 }
 
 
-psa_pmi <- function(word_list, output_dir, cv_sep) {
+psa_pmi <- function(fun, word_list, output_dir, cv_sep) {
 
   # Create an itnitial scoring matrix and a list of PSAs.
   s         <- MakeEditDistance(Inf)
   psa_list  <- PSAforEachWord(word_list, s, dist = T)
 
   # Update the scoring matrix using the PMI.
-  pmi_rlt  <- PairwisePMI(psa_list, word_list, s, UpdatePMI, cv_sep)
+  pmi_rlt  <- PairwisePMI(psa_list, word_list, s, fun, cv_sep)
   pmi_list <- pmi_rlt$pmi_list
   s        <- pmi_rlt$s
   psa_list <- pmi_rlt$psa_list
@@ -43,7 +43,7 @@ execute_psa <- function(method, word_list, output_dir, cv_sep=T) {
 
   psa_list <- switch(num,
                      "1" = psa_ld(word_list),
-                     "2" = psa_pmi(word_list, output_dir, cv_sep)
+                     "2" = psa_pmi(UpdatePMI, word_list, output_dir, cv_sep)
   )
 
   return(psa_list)
