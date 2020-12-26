c7a20caaccab0990723d7f6f2f8e48c3d118454a
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-26 18:15:54 +0900

    [modify] The 'MakePath' function do not make the directory to output the results.

--- R/lib/verif_lib/make_path.R
+++ R/lib/verif_lib/make_path.R
@@ -1,21 +1,18 @@
 MakePath <- function(file, dir, ext) {
-  
+
   if (is.na(ext)) {
     ext <- NULL
   } else {
     ext <- paste("_", ext, sep = "")
   }
-  
+
   path <- list()
-  
+
   # Set the path of the matching rate.
   path$ansrate.file <- paste("../../Alignment/", file, ext, "_", format(Sys.Date()), ".txt", sep = "")
-  
+
   # Set the path of the PSA directory.
   path$output.dir <- paste("../../Alignment/", dir, ext, "_", format(Sys.Date()), "/", sep = "")
-  if (!dir.exists(path$output.dir)) {
-    dir.create(path$output.dir)
-  }
-  
+
   return(path)
 }
