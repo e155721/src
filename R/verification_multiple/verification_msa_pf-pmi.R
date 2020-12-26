a38e36a8adfbd2ad71811a58ea617dd03ab3b0ad
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-26 20:37:45 +0900

    [refactor] Refactored the 'verification_msa_pf-pmi.R'.

--- R/verification_multiple/verification_msa_pf-pmi.R
+++ R/verification_multiple/verification_msa_pf-pmi.R
@@ -9,16 +9,28 @@ multiple <- "multiple_pf-pmi"
 ext <- commandArgs(trailingOnly = TRUE)[1]
 path <- MakePath(ansrate, multiple, ext)
 
-word_list <- make_word_list("../../Alignment/org_data/input.csv")
-gold_list <- make_word_list("../../Alignment/org_data/gold.csv")
+word_list      <- make_word_list("../../Alignment/org_data/input.csv")
+word_list_gold <- make_word_list("../../Alignment/org_data/gold.csv")
 
 pmi_rlt  <- msa_pf_pmi(word_list, cv_sep = T)
-pmi_list  <- pmi_rlt$pmi_list
+pmi_list <- pmi_rlt$pmi_list
 s        <- pmi_rlt$s
 msa_list <- pmi_rlt$msa_list
 
-# Calculate the accuracy of the MSAs.
-verification_msa(msa_list, gold_list, path$ansrate.file, path$output.dir)
+msa_list_gold <- lapply(word_list_gold, (function(x){
+  x_tmp             <- DelGap(list2mat(x))
+  attributes(x_tmp) <- list(dim = dim(x_tmp), word = attributes(x)$word)
+  y     <- list()
+  y$aln <- x_tmp
+  return(y)
+}))
+
+# Calculate the MSAs accuracy.
+verification_msa(msa_list, msa_list_gold, path$ansrate.file, path$output.dir)
+
+# Output the MSAs.
+output_msa(msa_list, path$output.dir, ".csv")
+output_msa(msa_list_gold, path$output.dir, "_lg.csv")
 
 # Save the matrix of the PMIs and the scoring matrix.
 rdata_path <- MakeMatPath("list_msa_pf-pmi", "score_msa_pf-pmi", ext)
