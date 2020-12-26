a53658bd89f3812b60c6bc4df7a70723e4d3b7ca
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-26 15:45:45 +0900

    [refactor] Refactored the 'verification_pmi.R'.

--- R/verification/verification_pmi.R
+++ R/verification/verification_pmi.R
@@ -10,16 +10,22 @@ dir <- "pairwise_pmi"
 ext <- commandArgs(trailingOnly = TRUE)[1]
 path <- MakePath(file, dir, ext)
 
-word_list <- make_word_list("../../Alignment/org_data/input.csv")
-gold_list <- make_word_list("../../Alignment/org_data/gold.csv")
+word_list      <- make_word_list("../../Alignment/org_data/input.csv")
+word_list_gold <- make_word_list("../../Alignment/org_data/gold.csv")
 
 pmi_rlt  <- psa_pmi(word_list, cv_sep = T)
-pmi_list  <- pmi_rlt$pmi_list
+pmi_list <- pmi_rlt$pmi_list
 s        <- pmi_rlt$s
 psa_list <- pmi_rlt$psa_list
 
-# Execute the PSA for each word.
-VerificationPSA(psa_list, gold_list, path$ansrate.file, path$output.dir)
+psa_list_gold <- lapply(word_list_gold, MakeGoldStandard)
+
+# Calculate the PSAs accuracy.
+VerificationPSA(psa_list, psa_list_gold, path$ansrate.file, path$output.dir)
+
+# Output the PSAs.
+output_psa(psa_list, output_dir = path$output.dir, ext = ".csv")
+output_psa(psa_list, output_dir = path$output.dir, ext = "_lg.csv")
 
 # Save the matrix of the PMIs and the scoring matrix.
 rdata_path <- MakeMatPath("list_psa_pmi", "score_psa_pmi", ext)
