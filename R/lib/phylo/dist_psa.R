60f8893a9039cdc2ff5785523214f0742e530a2c
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-28 23:33:39 +0900

    [fix] Fixed the order of the 'del_na' function call.

--- R/lib/phylo/dist_psa.R
+++ R/lib/phylo/dist_psa.R
@@ -142,8 +142,8 @@ make_region_dist <- function(word_list, method, s, output_dir) {
   if (!dir.exists(output_nnet)) dir.create(output_nnet)
 
   save(L, file = paste(output_nnet, "/", "aln_list_", method, ".RData", sep = ""))
-  diag(ldnd_mat) <- 0
   ldnd_mat <- del_na(ldnd_mat)
+  diag(ldnd_mat) <- 0
   save(ldnd_mat, file = paste(output_nnet, "/", "reg_dist_", method, ".RData", sep = ""))
 
   write.nexus.dist(ldnd_mat, file = paste(output_nnet, "/", "reg_dist_", method, ".nexus", sep = ""))
