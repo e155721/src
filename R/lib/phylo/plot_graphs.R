dee433f4feb4476a6ac67ceb153d378b27287d4a
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-31 08:18:09 +0900

    [modify] Modified the 'Plot' function.
    The output file name was modified.

--- R/lib/phylo/plot_graphs.R
+++ R/lib/phylo/plot_graphs.R
@@ -79,6 +79,8 @@ Plot <- function(psa_list, output_dir, method, s) {
   rlt <- foreach(i = 1:M) %dopar% {
 
     word <- attributes(psa_list[[i]])$word
+    word <- unlist(strsplit(word, split = "_"))
+    word <- paste(word[c(1, 3)], collapse = "_")  # combine the word ID and the assumed form.
 
     psa        <- psa_list[[i]]
     psa_dist   <- make_psa_dist(psa, s)
