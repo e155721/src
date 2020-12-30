2c1c0db080e7813a63ca930c4e784d3930186a1a
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2020-12-31 03:29:30 +0900

    [modify] Modified the 'make_word_list' function.
    The value of the 'word' attribute was modified.

--- R/lib/data_processing/make_word_list.R
+++ R/lib/data_processing/make_word_list.R
@@ -2,20 +2,23 @@ make_word_list <- function(file) {
 
   csv <- read.csv(file, fileEncoding = "UTF-8", stringsAsFactors = F)
 
-  word_vec <- sort(unique(csv[, 2]))
+  word_id <- sort(unique(csv[, 2]))
   word_list <- list()
-  M <- length(word_vec)
+  M <- length(word_id)
 
   for (i in 1:M) {
 
-    idx <- csv[, 2] == word_vec[i]
+    idx <- csv[, 2] == word_id[i]
+    assumed_form <- unique(csv[idx, 6])
+    concept      <- unique(csv[idx, 25])
+
     word_list[[i]] <- strsplit(csv[idx, 11], split = "_")
-    region_list    <- strsplit(csv[idx, 19], split = "_")
+    region_list    <- as.list(csv[idx, 19])
     N <- length(word_list[[i]])
     for (j in 1:N) {
       word_list[[i]][[j]] <- matrix(c(region_list[[j]], word_list[[i]][[j]]), nrow = 1)
     }
-    attributes(word_list[[i]]) <- list(word = word_vec[i])
+    attributes(word_list[[i]]) <- list(word = paste(word_id[i], "_", concept, "_", assumed_form, sep = ""))
 
   }
 
