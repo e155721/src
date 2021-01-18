9437914a58287fec4e9f25f98ece4833574d3fe6
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2021-01-19 01:04:29 +0900

    [modify] Added the 'psa_pf_pmi' function to the 'execute_psa.R'.

--- R/lib/exec_align/execute_psa.R
+++ R/lib/exec_align/execute_psa.R
@@ -42,18 +42,33 @@ psa_pf_pmi0 <- function(fun, word_list, output_dir, cv_sep) {
 }
 
 
+psa_pf_pmi <- function(fun, word_list, output_dir, cv_sep) {
+
+  # Create an itnitial scoring matrix and a list of PSAs.
+  s         <- MakeEditDistance(Inf)
+  psa_list  <- PSAforEachWord(word_list, s, dist = T)
+
+  # Update the scoring matrix using the PMI.
+  psa_rlt <- PairwisePMI(psa_list, word_list, s, fun, cv_sep)
+
+  return(psa_rlt)
+}
+
+
 execute_psa <- function(method, word_list, output_dir, cv_sep) {
 
   num <- switch(method,
                 "ld"     = 1,
                 "pmi"    = 2,
-                "pf-pmi0" = 3
+                "pf-pmi0" = 3,
+                "pf-pmi" = 4
   )
 
   psa_rlt <- switch(num,
                     "1" = psa_ld(word_list),
                     "2" = psa_pmi(UpdatePMI, word_list, output_dir, cv_sep),
-                    "3" = psa_pf_pmi0(UpdatePFPMI0, word_list, output_dir, cv_sep)
+                    "3" = psa_pf_pmi0(UpdatePFPMI0, word_list, output_dir, cv_sep),
+                    "4" = psa_pf_pmi(UpdatePFPMI, word_list, output_dir, cv_sep)
   )
 
   return(psa_rlt)
