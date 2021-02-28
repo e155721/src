7ab7647f9f777912403a00c5543d111d236e2c25
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2021-03-01 01:07:32 +0900

    [refactor] The phoneme features of a gap are included the feature files.

--- R/lib/phoneme/phoneme.R
+++ R/lib/phoneme/phoneme.R
@@ -1,21 +1,20 @@
-make_phone_vec <- function(feat.mat) {
-  vec <- dimnames(feat.mat)[[1]]
+make_phone_vec <- function(feat_mat) {
+  vec <- dimnames(feat_mat)[[1]]
   vec <- vec[-which(vec == "-")]  # removing the gap symbols
-  vec
+  return(vec)
 }
 
+
 make_feat_mat <- function(file) {
-  feat.mat <- read.table(file, fileEncoding = "utf-8")
-  feat.mat <- as.matrix(feat.mat)
+  feat_mat <- read.table(file, fileEncoding = "utf-8")
+  feat_mat <- as.matrix(feat_mat)
 
-  N <- dim(feat.mat)[2]
+  N <- dim(feat_mat)[2]
   for (j in 1:N) {
-    feat.mat[, j] <- gsub(pattern = "*[ ]*", replacement = "", feat.mat[, j])
+    feat_mat[, j] <- gsub(pattern = "*[ ]*", replacement = "", feat_mat[, j])
   }
-  gap <- matrix(rep("0", N), 1, N, dimnames = list("-"))
-  feat.mat <- rbind(feat.mat, gap)
 
-  feat.mat
+  return(feat_mat)
 }
 
 
