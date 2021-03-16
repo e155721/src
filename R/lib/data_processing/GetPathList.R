a2eee44272b965dc1b00ce1857079453d10a8079
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2021-03-16 16:32:50 +0900

    [modify] The 'GetPathList' function takes the 'pattern' argument.

--- R/lib/data_processing/GetPathList.R
+++ R/lib/data_processing/GetPathList.R
@@ -1,15 +1,16 @@
-GetPathList <- function(dir = "../../Alignment/org_data/") {
+GetPathList <- function(dir = "../../Alignment/org_data/", pattern=NULL) {
   # Gets the files path list from a specified directory.
-  # 
+  #
   # Args:
-  #  dir: A character string which the path of the directory you want to get the path of files.
-  # 
+  #  dir: a character string which the path of the directory you want to get the path of files.
+  #  pattern: a character string which the pattern of the reading files.
+  #
   # Returns:
   #  The list of path of files.
-  
-  input.files <- list.files(dir)
+
+  input.files <- list.files(dir, pattern = pattern)
   input.paths <- paste(dir, input.files, sep = "/")
-  
+
   # make the list of file paths
   path.list <- list()
   num.files <- length(input.files)
@@ -20,6 +21,6 @@ GetPathList <- function(dir = "../../Alignment/org_data/") {
     path.list[[i]]["input"] <- input.paths[[i]]
     path.list[[i]]["name"]  <- input.files[[i]]
   }
-  
+
   return(path.list)
 }
