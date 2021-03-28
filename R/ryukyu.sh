691e125e51a0f5a2c76211e7b06a7d7eac837b81
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2021-03-29 03:24:42 +0900

    [modify] The 'ryukyu.sh' executes the 'admixture.sh'.

--- R/ryukyu.sh
+++ R/ryukyu.sh
@@ -14,18 +14,23 @@ cd ${base}/r
 echo "MSA LD"
 Rscript execution/execution_msa.R "ld" "$input_file" "$output_dir" "T" >/dev/null
 echo
+./admixture.sh "${output_dir}/msa_ld"
 
 echo "MSA PMI"
 Rscript execution/execution_msa.R "pmi" "$input_file" "$output_dir" "T" >/dev/null
 echo
+./admixture.sh "${output_dir}/msa_pmi"
 
 echo "MSA PF-PMI1"
 Rscript execution/execution_msa.R "pf-pmi1" "$input_file" "$output_dir" "T" >/dev/null
 echo
+./admixture.sh "${output_dir}/msa_pf-pmi1"
 
 echo "MSA PF-PMI2"
 Rscript execution/execution_msa.R "pf-pmi2" "$input_file" "$output_dir" "T" >/dev/null
 echo
+./admixture.sh "${output_dir}/msa_pf-pmi2"
 
 echo "MSA PF-PMI3"
 Rscript execution/execution_msa.R "pf-pmi3" "$input_file" "$output_dir" "T" >/dev/null
+./admixture.sh "${output_dir}/msa_pf-pmi3"
