9d92d40334cabb58ba579eb02f768bd15fc5fb93
Yuta Miyagi <e155721@Yutas-MacBook-Pro.local>
2021-11-30 21:52:39 +0900

    [fix] Fixed about treating word IDs.

--- R/lib/data_processing/make_word_list.R
+++ R/lib/data_processing/make_word_list.R
@@ -1,25 +1,41 @@
+get_word_id <- function(x) {
+  paste(strsplit(x, "")[[1]][7:9], collapse = "")
+}
+
 make_word_list <- function(file) {
   
   ## CSV files are not read correctly on a container if the 'fileEncoding' is specified.
   csv <- read.csv(file, stringsAsFactors = F)
   
-  word_id <- sort(unique(csv[, 6]))
+  word_id_org_order <- unlist(lapply(csv[, 3], get_word_id))
+  word_id <- sort(unique(unlist(lapply(csv[, 3], get_word_id))))
   word_list <- list()
   M <- length(word_id)
   
+  assumed_form_all <- csv[, 6]
+  
+  word_no <- 1
+  
   for (i in 1:M) {
     
-    idx <- csv[, 6] == word_id[i]
+    idx <- word_id_org_order == word_id[i]
     assumed_form <- unique(csv[idx, 6])
-    concept      <- unique(csv[idx, 25])
-    
-    word_list[[i]] <- strsplit(csv[idx, 11], split = "_")
-    region_list    <- as.list(csv[idx, 23])
-    N <- length(word_list[[i]])
+
+    N <- length(assumed_form)
     for (j in 1:N) {
-      word_list[[i]][[j]] <- matrix(c(region_list[[j]], word_list[[i]][[j]]), nrow = 1)
+      csv_tmp <- csv[idx, , drop=F]
+      csv_tmp <- csv_tmp[assumed_form[j] == assumed_form_all, , drop=F]
+      csv_tmp <- csv_tmp[!is.na(csv_tmp[, 11]), , drop=F]
+  
+      word_list[[word_no]] <- strsplit(csv_tmp[, 11], split = "_")
+      region_list <- as.list(csv_tmp[, 23])
+      O <- length(word_list[[word_no]])
+      for (k in 1:O) {
+        word_list[[word_no]][[k]] <- matrix(c(region_list[[k]], word_list[[word_no]][[k]]), nrow = 1)
+      }
+      attributes(word_list[[word_no]]) <- list(word = paste(word_id[i], "_", assumed_form[j], sep = ""))
+      word_no <- word_no + 1
     }
-    attributes(word_list[[i]]) <- list(word = paste(assumed_form, "_", concept, sep = ""))
     
   }
   
